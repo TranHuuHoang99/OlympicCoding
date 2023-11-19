690a58186b0db079ffab4567cb6246ccfeb65d28
TranHuuHoang99 <thhoang08091999@gmail.com>
2023-11-19 23:43:08 +0700

    add new setup for competitive

--- main.cpp
+++ main.cpp
@@ -14,49 +14,53 @@ void _scan(char *value) {cin >> value;}
 #define GET_MIN(a ,b) (a < b ? a : b)
 #define scan_char(value) _scan(value)
 #define scan_arr(N, _array_type) {for(int i = 0; i < N; i++) {scan_char(_array_type[i]);};}
+#define loop(i, N) for(i; i < N; i++)
 
 int n = 0;
 
-int runCase(vector<int> arr) {
-    int ret = 0;
-    int min = INT32_MAX;
-    int max = INT32_MIN;
-    bool isUp = true;
+void solve(char *arr, int color_num) {
 
-    for(int i = 0; i < n; i++) {
-        if(isUp) {
-            if(arr[i] > max) {
-                max = arr[i];
-                if(i == n-1) ret += max;
-            } else {
-                ret += arr[i-1];
-                isUp = false;
-                min = arr[i];
-            }
-        } else {
-            if(arr[i] < min) {
-                min = arr[i];
-            } else {
-                ret -= arr[i-1];
-                if(i == n-1) ret += arr[i];
-                isUp = true;
-                max = arr[i];
-            }
-        }
-    }
+}
+
+int runCase(char *arr) {
+    int ret = 0;
+    const int len = n*n;
 
     return ret;
 }
 
 int main(void) {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(NULL);  
 
     scan_char(n);
-    vector<int> arr(n);
-    scan_arr(n, arr);
+
+    char temp[n];
+    char arr[n*n];
+
+    memset(temp, 0, n);
+    memset(arr, 0, n*n);
+
+    int j = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> temp;
+        memcpy(&arr[j], temp, n);
+        memset(temp, 0, n);
+        j += n;
+    }   
+
+#ifdef HOANG_DEBUG
+    auto start_pro = std::chrono::high_resolution_clock::now();
+#endif
 
     cout << runCase(arr) << endl;
+
+#ifdef HOANG_DEBUG
+    auto stop_pro = std::chrono::high_resolution_clock::now();
+    std::cout << "finished in : " << 
+    std::chrono::duration_cast<std::chrono::milliseconds>(stop_pro-start_pro).count()
+    << " ms \n";
+#endif
 
     return 0;
 }
