2e10e1b96f7a5a8ed812989b7bff13326769ee62
TranHuuHoang99 <thhoang08091999@gmail.com>
2023-12-18 23:14:41 +0700

    idea codepro solution 9

--- main.cpp
+++ main.cpp
@@ -23,80 +23,40 @@ void _scan(char *value) {cin >> value;}
 
 int n = 0;
 
-int runCase(vector<vector<int> > arr, vector<vector<bool> > visited, vector<vector<int> > ret_arr) {
-    queue<vector<int>> path;
-    vector<vector<int>> temp;
-    int index = 0;
+uint128_type runCase(vector<int> arr) {
+    uint128_type ret = 0;
     int min = INT32_MAX;
+    int max = INT32_MIN;
+    bool isUp = true;
+    vector<int> path;
 
-    int hor = 0;
-    int ver = 0;
-
-    path.push({0,0});
-    ret_arr[0][0] = 0;
-    visited[0][0] = true;
-
-    while(!path.empty()) {
-        ver = path.front()[0];
-        hor = path.front()[1];
-        path.pop();
-        index = 0;
-        min = INT32_MAX;
-
-        if(ver-1 >= 0) {
-            if(ret_arr[ver-1][hor] > ret_arr[ver][hor] + arr[ver-1][hor]) {
-                ret_arr[ver-1][hor] = ret_arr[ver][hor] + arr[ver-1][hor];
+    for(int i = 0; i < n; i++) {
+        if(isUp) {
+            if(arr[i] > max) {
+                max = arr[i];
+            } else {
+                path.push_back(i-1);
+                isUp = false;
+                min = arr[i];
             }
-            if(!visited[ver-1][hor]) {
-                visited[ver-1][hor] = true;
-                temp.push_back({ver-1, hor});
+        } else {
+            if(arr[i] < min) {
+                min = arr[i];
+            } else {
+                path.push_back(i-1);
+                isUp = true;
+                max = arr[i];
             }
-        }
-
-        if(ver+1 < n) {
-            if(ret_arr[ver+1][hor] > ret_arr[ver][hor] + arr[ver+1][hor]) {
-                ret_arr[ver+1][hor] = ret_arr[ver][hor] + arr[ver+1][hor];
-            }
-            if(!visited[ver+1][hor]) {
-                visited[ver+1][hor] = true;
-                temp.push_back({ver+1, hor});
-            }
-        }
-
-        if(hor-1 >= 0) {
-            if(ret_arr[ver][hor-1] > ret_arr[ver][hor] + arr[ver][hor-1]) {
-                ret_arr[ver][hor-1] = ret_arr[ver][hor] + arr[ver][hor-1];
-            }
-            if(!visited[ver][hor-1]) {
-                visited[ver][hor-1] = true;
-                temp.push_back({ver, hor-1});
-            }
-        }
-
-        if(hor+1 < n) {
-            if(ret_arr[ver][hor+1] > ret_arr[ver][hor] + arr[ver][hor+1]) {
-                ret_arr[ver][hor+1] = ret_arr[ver][hor] + arr[ver][hor+1];
-            }
-            if(!visited[ver][hor+1]) {
-                visited[ver][hor+1] = true;
-                temp.push_back({ver, hor+1});
-            }
-        }
-
-        for(int i = 0; i < temp.size(); i++) {
-            if(min > ret_arr[temp[i][0]][temp[i][1]]) {
-                min = ret_arr[temp[i][0]][temp[i][1]];
-                index = i;
-            }
-        }
-
-        if(min != INT32_MAX) {
-            path.push({temp[index][0], temp[index][1]});
-            temp.erase(temp.begin()+index);
         }
     }
 
-    return ret_arr[n-1][n-1];
+    if(path.back() != n-1 && arr[path.back()] > arr[n-1]) path.push_back(n-1);
+
+    for(int i = 1; i < path.size(); i++) {
+        ret += (path[i] - path[i-1]);
+    }
+
+    return ret;
 }
 
 int main(void) {
@@ -105,27 +65,14 @@ int main(void) {
     cout.tie(NULL);
 
     scan_char(n);
-    char temp[n];
-    vector<vector<int> > arr(n);
-    vector<vector<bool> > visited(n);
-    vector<vector<int> > ret_arr(n);
-    
-    for(int i = 0; i < n; i++) {
-        memset(temp, 0, n);
-        cin >> temp;
-
-        for(int j = 0; j < n; j++) {
-            arr[i].push_back(static_cast<int>(temp[j]-'0'));
-            visited[i].push_back(false);
-            ret_arr[i].push_back(INT32_MAX);
-        }
-    }
+    vector<int> arr(n, 0);
+    scan_arr(n, arr);
 
 #ifdef HOANG_DEBUG
     auto start_pro = std::chrono::high_resolution_clock::now();
 #endif
 
-    cout << runCase(arr, visited, ret_arr) << endl;
+    cout << runCase(arr) << endl;
 
 #ifdef HOANG_DEBUG
     auto stop_pro = std::chrono::high_resolution_clock::now();
