d766cb5dd7b44904c826141eac9fbe10a39b4ef4
TranHuuHoang99 <thhoang08091999@gmail.com>
2024-01-09 22:45:59 +0700

    codepro solution 11

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <memory>
 
 using namespace std;
 
@@ -14,102 +15,75 @@ void _scan(char *value) {cin >> value;}
 #define scan_arr(N, _array_type) {for(int i = 0; i < N; i++) {scan_char(_array_type[i]);};}
 #define loop(i, N) for(i; i < N; i++)
 
-struct Node {
-    int x = 0;
-    int y = 0;
-    Node *next = nullptr;
-
-    Node() : x(0), y(0), next(nullptr) {};
-};
-
-int ver[4] = {1,0,-1,0};
-int hor[4] = {0,1,0,-1};
-
 int n = 0;
 int m = 0;
-vector<vector<int>> arr;
 
-int solve(queue<Node*> q_, vector<vector<bool>> visited) {
-    int ret = INT32_MAX;
-    Node *temp = nullptr;
-    int x_ = 0;
-    int y_ = 0;
+struct Node {
+    int start;
+    vector<int> des;
+    vector<int> dis;
 
-    while(!q_.empty()) {
-        temp = q_.front();
-        q_.pop();
-        for(int i = 0; i < 4; i++) {
-            x_ = temp->x + ver[i];
-            y_ = temp->y + hor[i];
-            if(x_ < 0 || x_ >= n) continue;
-            if(y_ < 0 || y_ >= m) continue;
-            if(!visited[x_][y_]) {
-                arr[x_][y_] = arr[temp->x][temp->y] + 1;
-                visited[x_][y_] = true;
-                Node *cur = new Node();
-                cur->x = x_;
-                cur->y = y_;
-                q_.push(cur);
-            }
+    Node(int start_) : start(start_) {}
+    Node() : start(0) {}
+};
 
-            if(arr[x_][y_] == 1 && ret > arr[temp->x][temp->y]) {
-                ret = arr[temp->x][temp->y];
-            }
-        }
-    }   
+vector<shared_ptr<Node>> path;
 
-    return ret - 2;
-}
+int runCase() {
+    int ret = INT32_MAX;    
+    vector<bool> visited(n,false);  
+    vector<int> arr(n, INT32_MAX);
+    queue<shared_ptr<Node>> q;
+    
+    shared_ptr<Node> temp = nullptr;
+    vector<shared_ptr<Node>> get_min;
+    int min = INT32_MAX;
+    int max = INT32_MIN;
+    int index_min = 0;
+    
+    for(int k = 0; k < n; k++) {
+        visited = vector<bool>(n, false);
+        arr = vector<int>(n,INT32_MAX);
+        max = INT32_MIN;
+        q.push(path[k]);
+        arr[q.front()->start] = 0;
+        visited[q.front()->start] = true;
 
-int runCase(vector<int> start_point, vector<vector<bool>> visited) {
-    int ret = 0;
-    queue<Node*> q;
-    queue<Node*> q_;
-    Node *root = new Node();
+        while(!q.empty()) {
+            min = INT32_MAX;
+            temp = q.front();
+            q.pop();
 
-    root->x = start_point[0];
-    root->y = start_point[1];
-    arr[root->x][root->y] = 2;
-    q.push(root);
-
-    Node *temp = nullptr;
-    int x_ = 0;
-    int y_ = 0;
-
-    while(!q.empty()) {
-        temp = q.front();
-        q.pop();
-        for(int i = 0; i < 4; i++) {
-            x_ = temp->x + ver[i];
-            y_ = temp->y + hor[i];
-
-            if(x_ < 0 || x_ >= n) continue;
-            if(y_ < 0 || y_ >= m) continue;
-            if(arr[x_][y_] == 1) {
-                arr[x_][y_] = 2;
-                Node *cur = new Node();
-                cur->x = x_;
-                cur->y = y_;
-                q.push(cur);
-            } 
-
-            if(!visited[x_][y_]) {
-                arr[x_][y_] = arr[temp->x][temp->y] + 1;
-                if(arr[x_][y_] == 2) {
-                    cout << x_ << ", " << y_ << "\n";
+            for(int i = 0; i < temp->des.size(); i++) {
+                if(arr[temp->des[i]] > arr[temp->start] + temp->dis[i]) {
+                    arr[temp->des[i]] = arr[temp->start] + temp->dis[i];
                 }
-                visited[x_][y_] = true;
-                Node *cur = new Node();
-                cur->x = x_;
-                cur->y = y_;
-                q_.push(cur);
+
+                if(!visited[temp->des[i]]) {
+                    get_min.push_back(path[temp->des[i]]);
+                    visited[temp->des[i]] = true;
+                }
+            }
+
+            for(int i = 0; i < get_min.size(); i++) {
+                if(min > arr[get_min[i]->start]) {
+                    min = arr[get_min[i]->start];
+                    index_min = i;
+                }
+            }
+
+            if(!get_min.empty()) {
+                q.push(get_min[index_min]);
+                get_min.erase(get_min.begin()+index_min);
             }
         }
+
+        for(int i = 0; i < arr.size(); i++) {
+            if(max < arr[i]) max = arr[i];
+        }
+
+        if(ret > max) ret = max;
     }
-
-    delete temp;
-
-    ret = solve(q_, visited);
 
     return ret;
 }
@@ -121,31 +95,33 @@ int main(void) {
 
     scan_char(n);
     scan_char(m);
-
-    vector<int> temp(m,0);
-    vector<int> start_point(2,0);
-    vector<vector<bool>> visited;
-    vector<bool> t_visited(m, false);
+    
+    vector<int> temp(3,0);
 
     for(int i = 0; i < n; i++) {
-        arr.push_back(temp);
-        visited.push_back(t_visited);
-        for(int j = 0; j < m; j++) {
-            cin >> arr[i][j];
-            if(arr[i][j] == 1) {
-                start_point[0] = i;
-                start_point[1] = j;
-                visited[i][j] = true;
-            }
-        } 
+        path.push_back(make_shared<Node>());
+        path[i]->start = i;
+        path[i]->des.push_back(i);
+        path[i]->dis.push_back(0);
     }
-    
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> temp[j];
+        }
+
+        path[temp[0]-1]->des.push_back(temp[1]-1);
+        path[temp[0]-1]->dis.push_back(temp[2]);
+
+        path[temp[1]-1]->des.push_back(temp[0]-1);
+        path[temp[1]-1]->dis.push_back(temp[2]);
+    }
 
 #ifdef HOANG_DEBUG
     auto start_pro = std::chrono::high_resolution_clock::now();
 #endif
 
-    cout << runCase(start_point, visited) << endl;
+    cout << runCase() << endl;
 
 #ifdef HOANG_DEBUG
     auto stop_pro = std::chrono::high_resolution_clock::now();
