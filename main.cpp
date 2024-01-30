ff7ec24658168ca2d35dd5cc55d47d354c00a249
Ha Nhan <htnhan1099@gmail.com>
2024-01-30 22:55:41 +0700

    codepro solution 3 unfinished

--- main.cpp
+++ main.cpp
@@ -3,132 +3,102 @@
 
 using namespace std;
 
-void _scan(string &value) {cin >> value;}
-void _scan(int &value) {cin >> value;}
-void _scan(char &value) {cin >> value;}
-void _scan(char *value) {cin >> value;}
+int n = 0, m = 0;
 
+typedef long long ll;
 
-#define GET_MAX(a, b) (a > b ? a : b)
-#define GET_MIN(a ,b) (a < b ? a : b)
-#define scan_char(value) _scan(value)
-#define scan_arr(N, _array_type) {for(int i = 0; i < N; i++) {scan_char(_array_type[i]);};}
-#define loop(i, N) for(i; i < N; i++)
+int solve(vector<int> sys_num, vector<string> s, vector<string> d) {    
+    vector<char> ret_arr;
+    string ret_str;
+    int temp_sys = 0;
+    int count1 = 0, count2 = 0;
+    ll ret1 = 0, ret2 = 0, ret = 0;
+    int _sign1 = 1, _sign2 = 1;
 
-int n = 0;
-int m = 0;
+    for(int i = 0; i < n; i++) {
+        temp_sys = sys_num[i];
+        ret1 = 0;
+        ret2 = 0;
+        ret = 0;
+        ret_arr.clear();
+        count1 = 0;
+        count2 = 0;
+        _sign1 = 1; 
+        _sign2 = 1; 
 
-struct Node {
-    int start;
-    vector<int> des;
-    vector<int> dis;
-
-    Node(int start_) : start(start_) {}
-    Node() : start(0) {}
-};
-
-vector<shared_ptr<Node>> path;
-
-int runCase() {
-    int ret = INT32_MAX;    
-    vector<bool> visited(n,false);  
-    vector<int> arr(n, INT32_MAX);
-    queue<shared_ptr<Node>> q;
-    
-    shared_ptr<Node> temp = nullptr;
-    vector<shared_ptr<Node>> get_min;
-    int min = INT32_MAX;
-    int max = INT32_MIN;
-    int index_min = 0;
-    
-    for(int k = 0; k < n; k++) {
-        visited = vector<bool>(n, false);
-        arr = vector<int>(n,INT32_MAX);
-        max = INT32_MIN;
-        q.push(path[k]);
-        arr[q.front()->start] = 0;
-        visited[q.front()->start] = true;
-
-        while(!q.empty()) {
-            min = INT32_MAX;
-            temp = q.front();
-            q.pop();
-
-            for(int i = 0; i < temp->des.size(); i++) {
-                if(arr[temp->des[i]] > arr[temp->start] + temp->dis[i]) {
-                    arr[temp->des[i]] = arr[temp->start] + temp->dis[i];
-                }
-
-                if(!visited[temp->des[i]]) {
-                    get_min.push_back(path[temp->des[i]]);
-                    visited[temp->des[i]] = true;
+        for(int j = s[i].size()-1; j >= 0; j--) {  
+            if(s[i][count1] == '-') {
+                _sign1 = -1;
+            } else {
+                if(s[i][count1] >= 65) {
+                    ret1 += (static_cast<int>(s[i][count1] - 55) * pow(temp_sys, j));
+                } else {
+                    ret1 += (static_cast<int>(s[i][count1] - 48) * pow(temp_sys, j));
                 }
             }
-
-            for(int i = 0; i < get_min.size(); i++) {
-                if(min > arr[get_min[i]->start]) {
-                    min = arr[get_min[i]->start];
-                    index_min = i;
-                }
-            }
-
-            if(!get_min.empty()) {
-                q.push(get_min[index_min]);
-                get_min.erase(get_min.begin()+index_min);
-            }
+            count1++;
         }
 
-        for(int i = 0; i < arr.size(); i++) {
-            if(max < arr[i]) max = arr[i];
+        for(int j = d[i].size()-1; j >= 0; j--) {   
+            if(d[i][count2] == '-') {
+                _sign2 = -1;
+            } else {
+                if(d[i][count2] >= 65) {
+                    ret2 += (static_cast<int>(d[i][count2] - 55) * pow(temp_sys, j));
+                } else {
+                    ret2 += (static_cast<int>(d[i][count2] - 48) * pow(temp_sys, j));
+                }
+            }
+            count2++;            
         }
 
-        if(ret > max) ret = max;
+        ret = ret1 * ret2;
+        int temp_int = 0;
+        while(ret > 0) {
+            temp_int = ret % temp_sys;
+            temp_int = temp_int >= 10 ? temp_int + 55 : temp_int + 48;
+            ret_arr.push_back(static_cast<char>(temp_int));
+            if(ret_arr.back() == '0') ret_arr.pop_back();
+            ret /= temp_sys;
+        }
+
+        if((_sign1 * _sign2) == -1) {
+            ret_arr.push_back('-');
+        }
+
+        if(ret_arr.size() > 0) {
+            if(ret_arr.size() == 1 && ret_arr[0] == '-') {
+                ret_str = "0";
+            } else {
+                ret_str = string(ret_arr.rbegin(), ret_arr.rend());
+            }
+        } else {
+            ret_str = "0";
+        }
+       
+        cout << ret_str << endl;
     }
 
-    return ret;
+    return 0;
 }
 
 int main(void) {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);  
+    cin.tie(NULL);
     cout.tie(NULL);
 
-    scan_char(n);
-    scan_char(m);
-    
-    vector<int> temp(3,0);
+
+    cin >> n;
+
+    vector<int> sys_num(n, 0);
+    vector<string> s(n, " ");
+    vector<string> d(n, " ");
 
     for(int i = 0; i < n; i++) {
-        path.push_back(make_shared<Node>());
-        path[i]->start = i;
-        path[i]->des.push_back(i);
-        path[i]->dis.push_back(0);
+        cin >> sys_num[i] >> s[i] >> d[i];
     }
 
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < 3; j++) {
-            cin >> temp[j];
-        }
-
-        path[temp[0]-1]->des.push_back(temp[1]-1);
-        path[temp[0]-1]->dis.push_back(temp[2]);
-
-        path[temp[1]-1]->des.push_back(temp[0]-1);
-        path[temp[1]-1]->dis.push_back(temp[2]);
-    }
-
-#ifdef HOANG_DEBUG
-    auto start_pro = std::chrono::high_resolution_clock::now();
-#endif
-
-    cout << runCase() << endl;
-
-#ifdef HOANG_DEBUG
-    auto stop_pro = std::chrono::high_resolution_clock::now();
-    std::cout << "finished in : " << 
-    std::chrono::duration_cast<std::chrono::milliseconds>(stop_pro-start_pro).count()
-    << " ms \n";
-#endif
+    solve(sys_num, s, d);
 
     return 0;
 }
