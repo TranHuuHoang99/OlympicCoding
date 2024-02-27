be19064c5c01e685276b34f1d02804552a52e189
TranHuuHoang99 <thhoang08091999@gmail.com>
2024-02-28 00:15:07 +0700

    koko eating bananas leetcode

--- main.cpp
+++ main.cpp
@@ -3,71 +3,53 @@
 using namespace std;
 
 class Solution {
-    private:
-        vector<int> moves = {0,1,0,-1,0};
-
     public:
-        bool isValid(vector<vector<char>> &grid, vector<vector<bool>> &isVisited, int i, int j, int x, int y) {
-            isVisited[i][j] = true;
+        typedef long long ll;
+        int minEatingSpeed(vector<int>& piles, int h) {
+            ll left = 1;
+            ll right = *max_element(piles.begin(), piles.end());
+            int ret = 0;
 
-            for(int index = 0; index < 4; index++) {
-                int ver = i + moves[index];
-                int hor = j + moves[index+1];
-                if(
-                    ver >= 0 &&
-                    ver < grid.size() && 
-                    hor >= 0 &&
-                    hor < grid[0].size() && 
-                    grid[ver][hor] == grid[i][j] &&
-                    !(ver == x && hor == y)
-                ) {
-                    
-                    if(isVisited[ver][hor] || isValid(grid, isVisited, ver, hor, i, j)) {
-                        return true;
+            while(left <= right) {
+                ll mid = left+(right-left)/2;
+                ll count = 0;
+                for(auto p : piles) {
+                    if(p < mid) {
+                        count++;
+                    } else if(p % mid == 0) {
+                        count += (p/mid);
+                    } else {
+                        count += (p/mid+1);
                     }
                 }
-            }
 
-            return false;
-        }
-
-        bool containsCycle(vector<vector<char>>& grid) {
-            vector<vector<bool>> isVisited(grid.size(), vector<bool>(grid[0].size(), false));
-
-            for(int i = 0; i < grid.size(); i++) {
-                for(int j = 0; j < grid[i].size(); j++) {
-                    if(!isVisited[i][j] && isValid(grid, isVisited, i, j, -1, -1)) {
-                        return true;
-                    }
+                if(count <= h) { // there is some cases "count" do not equal to h so we update the value nearly to h
+                    ret = mid;
+                    right = mid -1;
+                } else {
+                    left = mid + 1;
                 }
             }
-
-            return false;
+ 
+            return ret;
         }
-};  
+};
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    vector<vector<vector<char>>> data = {
-        // {{'a','a','a','a'}, {'a','b','b','a'}, {'a','b','b','a'}, {'a','a','a','a'}},
-        // {{'c','c','c','a'},{'c','d','c','c'},{'c','c','e','c'},{'f','c','c','c'}},
-        // {{'a','b','b'},{'b','z','b'},{'b','b','a'}},
-        // {{'c','a','d'},{'a','a','a'},{'a','a','d'},{'a','c','d'},{'a','b','c'}},
-        {{'b','a','c'},{'c','a','c'},{'d','d','c'},{'b','c','c'}}
-    };
-
-    // b a c
-    // c a c 
-    // d d c
-    // b c c
-
     Solution solve;
 
+    vector<pair<int, vector<int>>> data = {
+        {8, {3,6,7,11}},
+        {5, {30,11,23,4,20}},
+        {6, {30,11,23,4,20}}
+    };
+
     for(auto d : data) {
-        cout << solve.containsCycle(d) << endl;
+        cout << solve.minEatingSpeed(d.second, d.first) << endl;
     }
 
     return 0;
