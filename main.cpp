3ffc0ecb9b4196ad59820407dd87d086f0f448fe
TranHuuHoang99 <thhoang08091999@gmail.com>
2024-03-04 00:09:02 +0700

    n queens need to check diagnal

--- main.cpp
+++ main.cpp
@@ -4,33 +4,52 @@ using namespace std;
 
 class Solution {
     public:
-        typedef long long ll;
-        int minEatingSpeed(vector<int>& piles, int h) {
-            ll left = 1;
-            ll right = *max_element(piles.begin(), piles.end());
-            int ret = 0;
+        vector<vector<int>> ret_int;
 
-            while(left <= right) {
-                ll mid = left+(right-left)/2;
-                ll count = 0;
-                for(auto p : piles) {
-                    if(p < mid) {
-                        count++;
-                    } else if(p % mid == 0) {
-                        count += (p/mid);
-                    } else {
-                        count += (p/mid+1);
-                    }
+        void dfs(int ver, int hor, const int &len, map<int,int> get_ret, vector<int> temp_ret) {
+            if(ver >= len-1) {
+                ret_int.push_back(temp_ret);
+                return;
+            }
+
+            for(int i = 0; i < len; i++) {
+                if(i-1 == hor || i+1 == hor || get_ret.find(i) != get_ret.end()) {
+                    continue;
                 }
 
-                if(count <= h) { // there is some cases "count" do not equal to h so we update the value nearly to h
-                    ret = mid;
-                    right = mid -1;
-                } else {
-                    left = mid + 1;
+                get_ret[i] = i;
+                temp_ret.push_back(i);
+                dfs(ver+1, i, len, get_ret, temp_ret);
+                get_ret.erase(i);
+                temp_ret.pop_back();
+            }
+
+            return;
+        }
+
+        vector<vector<string>> solveNQueens(int n) {    
+            string temp;
+            for(int i = 0; i < n; i++) temp += '.';
+            vector<vector<string>> ret;
+            map<int,int> get_ret;
+            vector<int> temp_ret;
+            
+            for(int i = 0; i < n; i++) {
+                get_ret[i] = i;
+                temp_ret.push_back(i);
+                dfs(0, i, n, get_ret, temp_ret);
+                get_ret.clear();
+                temp_ret.clear();
+            }
+
+            ret.assign(ret_int.size(), vector<string>(n, temp));
+
+            for(int i = 0; i < ret_int.size(); i++) {
+                for(int j = 0; j < ret_int[i].size(); j++) {
+                    ret[i][j][ret_int[i][j]] = 'Q';
                 }
             }
- 
+
             return ret;
         }
 };
@@ -41,15 +60,12 @@ int main(void) {
     cout.tie(NULL);
 
     Solution solve;
-
-    vector<pair<int, vector<int>>> data = {
-        {8, {3,6,7,11}},
-        {5, {30,11,23,4,20}},
-        {6, {30,11,23,4,20}}
-    };
-
-    for(auto d : data) {
-        cout << solve.minEatingSpeed(d.second, d.first) << endl;
+    
+    for(auto c : solve.solveNQueens(4)) {
+        for(auto sub_c : c) {
+            cout << sub_c << ", ";
+        }
+        cout << "\n";
     }
 
     return 0;
