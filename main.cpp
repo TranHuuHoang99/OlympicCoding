310511ff473c51b5ff6dc5507340ce3eeffc81b0
TranHuuHoang99 <thhoang08091999@gmail.com>
2024-03-11 22:58:40 +0700

    https://www.hackerrank.com/contests/dn-swpct-2024-batch-1/challenges/dory-love-food/submissions/code/1375494683 LGEDV SWPCT pretest batch 1 2024

--- main.cpp
+++ main.cpp
@@ -2,67 +2,74 @@
 
 using namespace std;
 
-typedef long long ll;
+vector<int> A;
+vector<int> B;
 
-int n = 0, x = 0;
+int n = 0, m = 0;
 
-struct Node {
-    int x;
-    int y;
-    int visits;
-    Node(int _x = 0, int _y = 0, int _visits = 0) : x(_x), y(_y), visits(_visits) {}
-};
+bool compare(pair<int,int> a, pair<int,int> b) {
+    if(a.first > b.first) {
+        return true;
+    } else if(a.first == b.first) {
+        return a.second <= b.second;
+    } else {
+        return false;
+    }
 
-vector<Node> path;
-vector<bool> isVisited;
-Node des;
-Node start;
-
-bool isValid(Node from, Node to) {
-    return abs(from.x - to.x) + abs(from.y - to.y) <= x;
+    return false;
 }
 
-int solve(void) {
-    queue<Node> q;
-    q.push(start);
+pair<int,int> solve(void) {
+    vector<int> temp;
+    vector<pair<int,int>> ret;
+    for(int i = 0; i < n; i++) {
+        temp = B;
+        temp[i] = 0;
+        vector<int> C(n, 0);
+        for(int j = 0; j < n; j++) {
+            C[j] = A[j] + temp[j];
+        }
 
-    Node temp;
-    while(!q.empty()) {
-        temp = q.front();
-        q.pop();
+        sort(C.begin(), C.end());
 
-        for(int i = 0; i <= n; i++) {
-            if(!isVisited[i] && isValid(temp, path[i])) {
-                isVisited[i] = true;
-                path[i].visits = temp.visits + 1;
-                q.push(path[i]);
-                if(path[i].x == des.x && path[i].y == des.y) return path[i].visits - 1;
+        int payment = 0;
+        int k = 0;
+        while(payment < m && k < n) {
+            payment += C[k];
+            if(payment > m) {
+                ret.push_back(pair<int,int>(k, payment-C[k]));
+            } else if(payment == m) {
+                ret.push_back(pair<int,int>(k+1, payment));
+            } else if(k >= n-1) {
+                ret.push_back(pair<int,int>(k+1, payment));
             }
+            
+            k++;
         }
     }
 
-    return -1;
+    sort(ret.begin(), ret.end(), compare);
+
+    return ret[0];
 }
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> n >> x;
-    cin >> des.x >> des.y;
-    des.visits = 0;
+    cin >> n >> m;
+    A.assign(n, 0);
+    B.assign(n, 0);
 
-    isVisited.assign(n+1, false);
-
-    int temp_x = 0;
-    int temp_y = 0;
     for(int i = 0; i < n; i++) {
-        cin >> temp_x >> temp_y;
-        path.push_back(Node(temp_x, temp_y, 0));
+        cin >> A[i];
     }
-    path.push_back(des);
 
-    cout << solve() << endl;
+    for(int i = 0; i < n; i++) {
+        cin >> B[i];
+    }
+
+    cout << solve().first << " " << solve().second << endl;
 
     return 0;
 }
