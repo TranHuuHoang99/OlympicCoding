ef127dde2513f208f252b6a960b9491e960ae126
TranHuuHoang99 <thhoang08091999@gmail.com>
2024-03-11 20:40:08 +0700

    https://www.hackerrank.com/contests/dn-swpct-2024-batch-1/challenges/road-to-company/submissions/code/1375486293 LGEDV SWPCT pretest batch 1 2024

--- main.cpp
+++ main.cpp
@@ -4,38 +4,63 @@ using namespace std;
 
 typedef long long ll;
 
-int n = 0, t = 0;
-vector<ll> A;
-vector<ll> S;
-vector<ll> arr;
+int n = 0, x = 0;
+
+struct Node {
+    int x;
+    int y;
+    int visits;
+    Node(int _x = 0, int _y = 0, int _visits = 0) : x(_x), y(_y), visits(_visits) {}
+};
+
+vector<Node> path;
+vector<bool> isVisited;
+Node des;
+Node start;
+
+bool isValid(Node from, Node to) {
+    return abs(from.x - to.x) + abs(from.y - to.y) <= x;
+}
 
 int solve(void) {
-    arr.assign(n, 0);
-    arr[n-1] = A[n-1] + t * S[n-1];
-    for(int i = n-2; i >= 0; i--) {
-        arr[i] = A[i] + t * S[i];
-        if(arr[i] > arr[i+1]) arr[i] = arr[i+1]; 
+    queue<Node> q;
+    q.push(start);
+
+    Node temp;
+    while(!q.empty()) {
+        temp = q.front();
+        q.pop();
+
+        for(int i = 0; i <= n; i++) {
+            if(!isVisited[i] && isValid(temp, path[i])) {
+                isVisited[i] = true;
+                path[i].visits = temp.visits + 1;
+                q.push(path[i]);
+                if(path[i].x == des.x && path[i].y == des.y) return path[i].visits - 1;
+            }
+        }
     }
 
-    set<ll> ret;
-    for(auto a : arr) {
-        ret.insert(a);
-    }
-
-    return ret.size();
+    return -1;
 }
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> n >> t;
-    A.assign(n, 0);
-    S.assign(n, 0);
+    cin >> n >> x;
+    cin >> des.x >> des.y;
+    des.visits = 0;
 
+    isVisited.assign(n+1, false);
+
+    int temp_x = 0;
+    int temp_y = 0;
     for(int i = 0; i < n; i++) {
-        cin >> A[i] >> S[i];
+        cin >> temp_x >> temp_y;
+        path.push_back(Node(temp_x, temp_y, 0));
     }
+    path.push_back(des);
 
     cout << solve() << endl;
 
