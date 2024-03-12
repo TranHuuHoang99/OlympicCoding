026857f7e37af20ecdbfcd0162115ff184eb91d9
TranHuuHoang99 <thhoang08091999@gmail.com>
2024-03-12 23:15:02 +0700

    Build a rockery lgedvoj 0.03s

--- main.cpp
+++ main.cpp
@@ -2,16 +2,23 @@
 
 using namespace std;
 
-vector<int> A;
-vector<int> B;
+struct Node {
+    pair<int,int> pos;
+    int value;
+    Node(pair<int,int> _pos, int _value) : pos(_pos), value(_value) {}
+};
 
-int n = 0, m = 0;
+int n = 0;
+vector<vector<int>> lands;
+set<int> walls;
+vector<int> moves = {-1,0,1,0,-1};
+vector<Node> nodes;
 
 bool compare(pair<int,int> a, pair<int,int> b) {
     if(a.first > b.first) {
         return true;
-    } else if(a.first == b.first) {
-        return a.second <= b.second;
+    } else if(a == b) {
+        return a.second < b.second;
     } else {
         return false;
     }
@@ -19,57 +26,66 @@ bool compare(pair<int,int> a, pair<int,int> b) {
     return false;
 }
 
-pair<int,int> solve(void) {
-    vector<int> temp;
+int solve(void) {
     vector<pair<int,int>> ret;
-    for(int i = 0; i < n; i++) {
-        temp = B;
-        temp[i] = 0;
-        vector<int> C(n, 0);
-        for(int j = 0; j < n; j++) {
-            C[j] = A[j] + temp[j];
-        }
+    queue<pair<int,int>> q;
+    vector<vector<bool>> temp_visited(n, vector<bool>(n, false));
+    vector<vector<bool>> isVisited;
 
-        sort(C.begin(), C.end());
+    for(auto w : walls) {
+        int count = 0;
+        isVisited = temp_visited;
+        pair<int,int> temp;
 
-        int payment = 0;
-        int k = 0;
-        while(payment < m && k < n) {
-            payment += C[k];
-            if(payment > m) {
-                ret.push_back(pair<int,int>(k, payment-C[k]));
-            } else if(payment == m) {
-                ret.push_back(pair<int,int>(k+1, payment));
-            } else if(k >= n-1) {
-                ret.push_back(pair<int,int>(k+1, payment));
+        for(auto node : nodes) {
+            if(!isVisited[node.pos.first][node.pos.second] && node.value > w) {
+                count++;
+                q.push({node.pos.first, node.pos.second});
+                isVisited[node.pos.first][node.pos.second] = true;
             }
-            
-            k++;
+
+            while(!q.empty()) {
+                temp = q.front();
+                q.pop();
+
+                for(int i = 0; i < 4; i++) {
+                    int ver = temp.first + moves[i];
+                    int hor = temp.second + moves[i+1];
+
+                    if(ver < 0 || ver >= n || hor < 0 || hor >= n) continue;
+
+                    if(!isVisited[ver][hor] && lands[ver][hor] > w) {
+                        q.push({ver, hor});
+                        isVisited[ver][hor] = true;
+                    }
+                }
+            }   
         }
+
+        ret.push_back({count, w});
     }
 
     sort(ret.begin(), ret.end(), compare);
 
-    return ret[0];
+    return ret[0].second;
 }
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> n >> m;
-    A.assign(n, 0);
-    B.assign(n, 0);
+    cin >> n;
+    lands.assign(n, vector<int>(n, 0)); 
 
     for(int i = 0; i < n; i++) {
-        cin >> A[i];
+        for(int j = 0; j < n; j++) {
+            cin >> lands[i][j];
+            walls.insert(lands[i][j]);
+            nodes.push_back(Node({i,j}, lands[i][j]));
+        }
     }
 
-    for(int i = 0; i < n; i++) {
-        cin >> B[i];
-    }
-
-    cout << solve().first << " " << solve().second << endl;
+    cout << solve() << endl;
 
     return 0;
 }
