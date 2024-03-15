a2c6d48d82a11442331212e9f31b62ea6b7cbeae
TranHuuHoang99 <thhoang08091999@gmail.com>
2024-03-15 20:05:55 +0700

    lgedvoj wick and pencils

--- main.cpp
+++ main.cpp
@@ -1,28 +1,27 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+typedef long long ll;
 int n = 0;
-vector<int> A;
-vector<int> B;
+vector<ll> pencils;
 
-int solve(void) {
-    int ret = INT_MIN;
+bool isValid(const int &i, const int &j, const int &k) {
+    return pencils[i] + pencils[j] > pencils[k];
+}
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            vector<int> temp_a(A.begin(), A.end());
-            vector<int> temp_b(B.begin(), B.end());
-            temp_a.erase(temp_a.begin()+i);
-            temp_b.erase(temp_b.begin()+j);
-            int count = 0;
-            for(int k = 0; k < n-1; k++) {
-                if(temp_a[k] == temp_b[k]) {
-                    count++;
-                } else {
-                    count = 0;
-                }
-                ret = max(ret, count);
+ll solve(void) {   
+    ll ret = 0;
+    sort(pencils.begin(), pencils.end());
+
+    for(int i = n-1; i >= 0; i--) {
+        int left = 0;
+        int right = i-1;
+        while(left < right) {
+            if(isValid(left, right, i)) {
+                ret += (right-left);
+                right--;
+            } else {
+                left++;
             }
         }
     }
@@ -33,21 +32,14 @@ int solve(void) {
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     cin >> n;
-    A.assign(n, 0);
-    B.assign(n, 0);
-
+    pencils.assign(n, 0);
     for(int i = 0; i < n; i++) {
-        cin >> A[i];
-    }
-
-    for(int i = 0; i < n; i++) {
-        cin >> B[i];
+        cin >> pencils[i];
     }
 
     cout << solve() << endl;
-
 
     return 0;
 }
