36d24ff1a55bf8b87df1626f75502cb1eb5ddd50
TranHuuHoang99 <thhoang08091999@gmail.com>
2024-03-28 21:28:52 +0700

    Best solutions for Lucky money (Easy) lgedvoj

--- main.cpp
+++ main.cpp
@@ -5,28 +5,48 @@ using namespace std;
 typedef long long ll;
 
 int n = 0;
-vector<ll> prices;
-ll max_pay = 0;
+vector<pair<int,int>> lm;
+vector<pair<int,int>> chance;
+vector<pair<int,int>> no_chance;
+
+bool cmp(pair<int,int> a, pair<int,int> b) {
+    return a.first > b.first;
+}
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     cin >> n;
-    prices.assign(n, 0);
+    lm.assign(n, {0,0});
+    int ret = 0;
+    int length = 0;
 
     for(int i = 0; i < n; i++) {
-        cin >> prices[i];
-        max_pay += prices[i];
+        cin >> lm[i].first >> lm[i].second;
+        if(lm[i].second > 0) {
+            chance.push_back(lm[i]);
+            ret += lm[i].first;
+            length += lm[i].second;
+        } else {
+            no_chance.push_back(lm[i]);
+        }
     }
 
-    sort(prices.begin(), prices.end(), greater<ll>());
+    if(!no_chance.empty()) sort(no_chance.begin(), no_chance.end(), cmp);
+    length -= (chance.size() - 1);
 
-    for(int i = 0; i < n; i++) {
-        if((i+1)%3 == 0) max_pay -= prices[i];
+
+    int i = 0;
+    while(length > 0) {
+        ret += no_chance[i].first;
+        length--;
+        i++;
+
+        if(i >= no_chance.size()) break;
     }
 
-    cout << max_pay << endl;
+    cout << ret << endl;
 
     return 0;
 }
