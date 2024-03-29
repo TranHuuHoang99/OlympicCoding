d54bb7fd1d6ee45584c8c0ebc45b9c39778ec7c5
TranHuuHoang99 <thhoang08091999@gmail.com>
2024-03-29 22:31:23 +0700

    Simple binary search 1 (Easy) lgedvoj

--- main.cpp
+++ main.cpp
@@ -5,31 +5,49 @@ using namespace std;
 typedef long long ll;
 
 int n = 0;
-ll a = 0, b = 0, c = 0;
-vector<pair<ll,ll>> arr;
+int Q = 0;
+vector<ll> K;
+vector<ll> arr;
 
-bool cmp(pair<ll,ll> a, pair<ll,ll> b) {
-    return a.first < b.first;
-}
+void bs(ll k) {
+    int left = 0;
+    int right = n-1;
 
-void solve(void) {
-    cin >> n >> c;
-    arr.assign(n, {0,0});
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i].first >> arr[i].second;
-    }
-
-    sort(arr.begin(), arr.end(), cmp);
-
-    int count = 0;
-    for(auto a : arr) {
-        if(a.first <= c) {
-            c += a.second;
-            count++;
+    while(left <= right) {
+        int mid = left + (right-left)/2;
+        if(arr[mid] == k) {
+            cout << "YES" << endl;
+            return;
+        } else if(arr[mid] < k) {
+            left = mid + 1;
+        } else {
+            right = mid-1;
         }
     }
 
-    cout << count << endl;
+    cout << "NO" << endl;
+    return;
+}
+
+void solve(void) {
+    cin >> n;
+    arr.assign(n, 0);
+    for(auto &a : arr) {
+        cin >> a;
+    }
+    sort(arr.begin(), arr.end());
+
+    cin >> Q;
+    K.assign(Q, 0);
+    for(auto &_k : K) {
+        cin >> _k;
+    }
+
+    for(auto _k : K) {
+        bs(_k);
+    }
+
+    return;
 }   
 
 int main(void) {
