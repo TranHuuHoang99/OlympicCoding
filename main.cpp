997e1a13edd9618fede4a9e09549c4214bab48f4
TranHuuHoang99 <thhoang08091999@gmail.com>
2024-04-04 00:12:50 +0700

    Slick (Medium) lgedvoj

--- main.cpp
+++ main.cpp
@@ -2,24 +2,30 @@
 
 using namespace std;
 
-int n = 0, m = 0;
-vector<vector<int>> matrix;
-vector<vector<bool>> isVisited;
-vector<pair<int,int>> sea;
 vector<int> moves = {1,0,-1,0,1};
 
-void solve(void) {   
-    map<int,int> ret;
+void solve(int n, int m) {
+    int slicks = 0;
+    vector<vector<int>> matrix(n, vector<int>(m,0));
+    vector<vector<bool>> isVisited(n, vector<bool>(m, false));
+    vector<pair<int,int>> sea;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> matrix[i][j];
+            sea.push_back({i,j});
+        }
+    }
+
     queue<pair<int,int>> q;
     pair<int,int> temp;
-    int slicks = 0;
+    map<int,int> ret;
 
     for(auto s : sea) {
         if(isVisited[s.first][s.second] || matrix[s.first][s.second] == 0) continue;
-        int count = 0;
-        slicks++;
         q.push({s.first, s.second});
         isVisited[s.first][s.second] = true;
+        int count = 0;
 
         while(!q.empty()) {
             temp = q.front();
@@ -29,42 +35,35 @@ void solve(void) {
             for(int i = 0; i < 4; i++) {
                 int ver = temp.first + moves[i];
                 int hor = temp.second + moves[i+1];
-
-                if(ver >= 0 && ver < n && hor >= 0 && hor < m) {
-                    if(!isVisited[ver][hor] && matrix[ver][hor] != 0) {
+                
+                if(ver >= 0 && ver < n && hor >= 0 && hor < m && !isVisited[ver][hor]) {
+                    isVisited[ver][hor] = true;
+                    if(matrix[ver][hor] == 1) {
                         q.push({ver, hor});
-                        isVisited[ver][hor] = true;
                     }
                 }
             }
         }
-        ret[count] +=1;
+
+        ret[count] += 1;
+        slicks++;
     }
 
     cout << slicks << endl;
     for(auto r : ret) {
         cout << r.first << " " << r.second << endl;
     }
-
-    return;
 }
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> n >> m;
-    matrix.assign(n, vector<int>(m, 0));   
-    isVisited.assign(n, vector<bool>(m, false));
-    
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            cin >> matrix[i][j];
-            sea.push_back({i,j});
-        }
-    }
+    int n = 0, m = 0;
 
-    solve();
+    while(cin >> n >> m && n && m) {
+        solve(n, m);
+    }
 
     return 0;
 }
