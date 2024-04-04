f462379deb9d25c20bab5b5ebd2036e695a2be15
TranHuuHoang99 <thhoang08091999@gmail.com>
2024-04-04 21:43:25 +0700

    Rising of sea level 2 (easy version) (Easy) lgedvoj

--- main.cpp
+++ main.cpp
@@ -3,63 +3,65 @@
 using namespace std;
 
 int n = 0, m = 0;
-vector<bool> isVisited;
-map<int,set<int>> cows;
-set<int> ret;
+vector<pair<int,int>> lands;
+vector<vector<int>> matrix;
+set<int> sea_level;
+vector<int> moves = {1,0,-1,0,1};
+
+int solve(void) {
+    priority_queue<int,vector<int>, greater<int>> ret;
+
+    for(auto sl : sea_level) {  
+        vector<vector<bool>> isVisited(n, vector<bool>(m, false));
+        queue<pair<int,int>> q;
+        pair<int,int> temp;
+        int count = 0;
+
+        for(auto l : lands) {
+            if(matrix[l.first][l.second] <= sl || isVisited[l.first][l.second]) continue;
+            q.push({l.first, l.second});
+            count++;
+            isVisited[l.first][l.second] = true;
+
+            while(!q.empty()) {
+                temp = q.front();
+                q.pop();
+
+                for(int i = 0; i < 4; i++) {
+                    int ver = temp.first + moves[i];
+                    int hor = temp.second + moves[i+1];
+
+                    if(ver >= 0 && ver < n && hor >= 0 && hor < m) {
+                        if(!isVisited[ver][hor] && matrix[ver][hor] > sl) {
+                            isVisited[ver][hor] = true;
+                            q.push({ver,hor});
+                        }
+                    }
+                }
+            }
+
+            if(count > 1) ret.push(sl);
+        }
+    }
+
+    return ret.empty() ? -1 : ret.top();
+}
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     cin >> n >> m;
-    isVisited.assign(n+1, false);
-    int cow1 = 0;
-    int cow2 = 0;
-    set<int> temp_s;
-    for(int i = 1; i <= n; i++) {
-        cows[i] = temp_s;
-    }
-
-    for(int i = 0; i < m; i++) {
-        cin >> cow1 >> cow2;
-        cows[cow1].insert(cow2);
-        cows[cow2].insert(cow1);
-    }
-
-    queue<int> q;
-    int temp = 0;
-    isVisited[1] = true;
-
-    for(auto s : cows[1]) {
-        q.push(s);
-    }
-    
-    while(!q.empty()) {
-        temp = q.front();
-        q.pop();
-        isVisited[temp] = true;
-
-        for(auto s : cows[temp]) {
-            if(!isVisited[s]) {
-                isVisited[s] = true;
-                q.push(s);
-            }
+    matrix.assign(n, vector<int>(m, 0));
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> matrix[i][j];
+            lands.push_back({i,j});
+            sea_level.insert(matrix[i][j]);
         }
     }
 
-    bool isValid = true;
-    for(int i = 1; i <= n; i++) {
-        if(!isVisited[i]) {
-            cout << i << " ";
-            isValid = false;
-        }
-    }
-    
-    if(isValid) {
-        cout << 0 << endl;
-    } else {
-        cout << "\n";
-    }
+    cout << solve() << endl;
 
     return 0;
 }
