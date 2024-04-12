b50728be295e54da6c3f2c15fccb1cc7974d1c53
hoang2.tran <thhoang08091999@gmail.com>
2024-04-12 22:36:02 +0700

    lgedvoj binladen

--- main.cpp
+++ main.cpp
@@ -2,84 +2,86 @@
 
 using namespace std;
 
-class Solution {
-    struct Node {
-        char name;
-        pair<int,int> pos;
-        Node(char _name, pair<int,int> _pos) : name(_name), pos(_pos) {}
-    };
-
-    public:
-        bool containsCycle(vector<vector<char>> &grid) {
-            vector<pair<int,int>> moves = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-            map<char, Node*> _map;
-            queue<Node*> q;
-            Node *temp = nullptr;
-            pair<int,int> steady_pos(0,0);
-            int n = grid.size();
-            int m = grid[0].size();
-            vector<vector<bool>> isVisited(n, vector<bool>(m, false));
-
-            for(int i = 0; i < grid.size(); i++) {
-                for(int j = 0; j < grid[i].size(); j++) {
-                    if(_map.find(grid[i][j]) == _map.end()) {
-                        _map[grid[i][j]] = new Node(grid[i][j], {i,j});
-                    }
-                }
-            }
-
-            for(auto sub_m : _map) {
-                q.push(sub_m.second);
-                steady_pos = sub_m.second->pos;
-                int count = 0;
-
-                while(!q.empty()) {
-                    temp = q.front();
-                    count++;
-                    isVisited[temp->pos.first][temp->pos.second] = true;
-                    q.pop();
-
-                    for(int i = 0; i < 4; i++) {
-                        int ver = temp->pos.first + moves[i].first;
-                        int hor = temp->pos.second + moves[i].second;
-
-                        if(ver < 0 || ver >= n || hor < 0 || hor >= m) continue;
-
-                        if(grid[ver][hor] == temp->name && ver == steady_pos.first && hor == steady_pos.second && count >= 4) {
-                            return true;
-                        }
-
-                        if(!isVisited[ver][hor] && grid[ver][hor] == temp->name) {
-                            q.push(new Node(temp->name, {ver, hor}));
-                            isVisited[ver][hor] = true;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            return false;
-        }
+struct Node {
+    int x,y,value;
+    bool operator < (const Node &a) const {
+        return a.value < value;
+    }
 };
+
+int M,N;
+vector<vector<int>> A;
+vector<vector<int>> B;
+vector<vector<int>> ret;
+vector<pair<int,int>> moves = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+int solve(void) {
+    int res = INT32_MAX;
+    priority_queue<Node> q;
+    ret[M][N-1] = 0;
+    q.push({M,N-1, ret[M][N-1]});
+
+    while(!q.empty()) {
+        auto [x,y,value] = q.top();
+        q.pop();
+
+        if(x == 0) res = min(res, value);
+
+        for(int i = 0; i < moves.size(); i++) {
+            int ver = x + moves[i].first;
+            int hor = y + moves[i].second;
+
+            if(ver >= 0 && ver <= M && hor >= 0 && hor < N) {
+                if(i == 0) {
+                    if(ret[ver][hor] > value + A[ver][hor]) {
+                        ret[ver][hor] = value + A[ver][hor];
+                        q.push({ver,hor, ret[ver][hor]});
+                    }
+                } else if(i ==1) {
+                    if(ret[ver][hor] > value + A[ver+1][hor]) {
+                        ret[ver][hor] = value + A[ver+1][hor];
+                        q.push({ver,hor,ret[ver][hor]});
+                    }
+                } else if(i == 2) {
+                    if(ret[ver][hor] > value + B[ver][hor-1]) {
+                        ret[ver][hor] = value + B[ver][hor-1];
+                        q.push({ver,hor,ret[ver][hor]});
+                    }
+                } else {
+                    if(ret[ver][hor] > value + B[ver][hor]) {
+                        ret[ver][hor] = value + B[ver][hor];
+                        q.push({ver,hor,ret[ver][hor]});
+                    }
+                }
+            }
+        }
+    }
+
+    return res;
+}
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
 
-    vector<vector<vector<char>>> data = {
-        {{'a','a','a','a'}, {'a','b','b','a'}, {'a','b','b','a'}, {'a','a','a','a'}},
-        {{'c','c','c','a'},{'c','d','c','c'},{'c','c','e','c'},{'f','c','c','c'}},
-        {{'a','b','b'},{'b','z','b'},{'b','b','a'}},
-        {{'c','a','d'},{'a','a','a'},{'a','a','d'},{'a','c','d'},{'a','b','c'}}
-    };
+    cin >> M >> N;
+    A.assign(M+1, vector<int>(N, 0));
+    B.assign(M+1, vector<int>(N-1,0));
+    ret.assign(M+1, vector<int>(N, INT32_MAX));
 
-    Solution solve;
-
-    for(auto d : data) {
-        cout << solve.containsCycle(d) << endl;
+    for(int i = 1; i <= M*2; i++) {
+        if(i%2 == 0) {
+            for(int j = 0; j < N-1; j++) {
+                cin >> B[i/2][j];
+            }
+        } else {
+            for(int j = 0; j < N; j++) {
+                cin >> A[(i+1)/2][j];
+            }
+        }
     }
 
+    cout << solve() << endl;
 
     return 0;
 }
