6ddbbf213e32b6bba5f8ff39c6116ad8e7374110
hoang2.tran <thhoang08091999@gmail.com>
2024-04-27 17:24:12 +0700

    SPOJ ABA12C - Buying Apples!

--- main.cpp
+++ main.cpp
@@ -2,62 +2,45 @@
 
 using namespace std;
 
-int N, M;
-vector<vector<int>> matrix;
-vector<pair<int,int>> moves = {
-    {1,0},{-1,0},{0,1},{0,-1},{-1,1},{1,1},{1,-1},{-1,-1}
-};
+int N, K;
+vector<int> prices;
+vector<int> res;
+vector<int> dp;
 
-vector<pair<int,int>> edges;
-
-int solve(void) {
-    int ret = 0;
-    queue<pair<int,int>> q;
-    vector<vector<bool>> isVisited(N, vector<bool>(M, false));
-
-    for(auto e : edges) {
-        if(isVisited[e.first][e.second]) continue;
-        q.push({e.first, e.second});
-        isVisited[e.first][e.second] = true;
-        bool isValid = true;
-
-        while(!q.empty()) {
-            auto [x,y] = q.front();
-            q.pop();
-
-            for(int i = 0; i < 8; i++) {
-                int ver = x + moves[i].first;
-                int hor = y + moves[i].second;
-
-                if(ver < 0 || ver > N-1 || hor < 0 || hor > M-1) continue;
-                if(matrix[ver][hor] > matrix[x][y]) isValid = false;
-                if(matrix[x][y] == matrix[ver][hor] && !isVisited[ver][hor]) {
-                    q.push({ver,hor});
-                    isVisited[ver][hor] = true;
-                }
+void solve(vector<int> dp) {
+    dp[0] = 0;
+    for(int i = 0; i <= K; i++) {
+        for(int j = 1; j <= K; j++) {
+            if(i+j > K) break;
+            if(prices[j] < 0) continue;
+            if(dp[i] != 1001) {
+                dp[i+j] = min(dp[i+j], dp[i] + prices[j]);
             }
         }
-
-        if(isValid) ret++;
     }
-
-    return ret;
-}
+    
+    dp[K] = dp[K] == 1001 ? -1 : dp[K];
+    res.push_back(dp[K]);
+}   
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> N >> M;
-    matrix.assign(N, vector<int>(M, 0));
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < M; j++) {
-            cin >> matrix[i][j];
-            edges.push_back({i,j});
-        }
+    int T;
+    cin >> T;
+
+    while(T--) {
+        cin >> N >> K;
+        prices.assign(K+1,0);
+        for(int i = 1; i <= K; i++) cin >> prices[i];
+        dp.assign(K+1, 1001);
+        solve(dp);
     }
 
-    cout << solve() << endl;
+    for(auto r : res) {
+        cout << r << "\n";
+    }
 
     return 0;
 }
