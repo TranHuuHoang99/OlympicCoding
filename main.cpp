6f214604faa2391c271d48e0bf27c3c477b393b9
hoang2.tran <thhoang08091999@gmail.com>
2024-05-06 21:54:44 +0700

    SPOJ ADAINDEX - Ada and Indexing

--- main.cpp
+++ main.cpp
@@ -3,49 +3,43 @@
 
 using namespace std;
 
-vector<int> res;
-string A;
-int N;
-vector<string> adp = {
-    "and", "in", "on", "at", "to", "of", "from", "for", "with"
+struct Node {
+    map<char,pair<Node*, int>> next;
+    Node() {}
 };
 
-bool cmp(string A, string B) {
-    return strcmp(A.c_str(), B.c_str()) == 0;
+int N, Q;
+Node *root = new Node();
+vector<int> res;
+
+void make_node(string str) {
+    Node *temp = root;
+
+    for(auto c : str) {
+        if(temp->next.find(c) == temp->next.end()) {
+            temp->next[c].first = new Node();
+        } 
+        temp->next[c].second++;
+
+        temp = temp->next[c].first;
+    }
 }
 
-void solve(vector<vector<string>> W) {
-    queue<string> q;
-    vector<string> dp;
-    for(string _w : W[0]) {
-        if(cmp(_w, A.substr(0, _w.size()))) {
-            q.push(_w);
+void solve(string str) {
+    int ret = 0;
+    Node *temp = root;
+
+    for(auto c : str) {
+        if(temp->next.find(c) != temp->next.end()) {    
+            ret = temp->next[c].second;
+            temp = temp->next[c].first;
+        } else {
+            ret = 0;
+            break;
         }
     }
 
-    for(int i = 1; i < N; i++) {
-        dp.clear();
-        while(!q.empty()) {
-            string temp = q.front();
-            q.pop();
-
-            for(string w : W[i]) {
-                string next = temp + w;
-                if(!dp.empty() && cmp(dp.back(), next)) continue;
-                if(cmp(next, A.substr(0,next.size()))) {
-                    dp.push_back(next);
-                }
-            }
-        }
-        for(string _dp : dp) q.push(_dp);
-    }
-
-    int count = 0;
-    for(string _dp : dp) {
-        if(cmp(_dp, A)) count++; 
-    }
-
-    res.push_back(count);
+    res.push_back(ret);
 }
 
 int main(void) {
@@ -57,30 +51,16 @@ int main(void) {
     freopen("input.txt", "r", stdin);
 #endif // HOANG_DEBUG   
 
-    int T;
-    cin >> T;
-    while(T--) {
-        cin >> N;
-        cin >> A;
-        vector<vector<string>> W(N);
-        string temp;
-        for(int i = 0; i < N; i++) {
-            cin >> temp;
-            int add = 0;
-            for(auto _adp : adp) {
-                if(strcmp(temp.c_str(), _adp.c_str()) == 0) {
-                    add = 1;
-                    break;
-                }
-            }
+    cin >> N >> Q;
+    string temp;
+    for(int i = 0; i < N; i++) {
+        cin >> temp;
+        make_node(temp);
+    }
 
-            W[i].assign(temp.size()+add, "");
-            for(int j = add; j < temp.size()+add; j++) {
-                W[i][j] = temp.substr(0,j+1-add);
-            }
-        }
-
-        solve(W);
+    for(int i = 0; i < Q; i++) {
+        cin >> temp;
+        solve(temp);
     }
 
     for(auto r : res) {
