b83b14e0feef625de6ffeb7d2031882f0caa78ff
hoang2.tran <thhoang08091999@gmail.com>
2024-05-27 22:55:48 +0700

    [SWPCT][HACKERRANK] information spread (batch 2 2024)

--- main.cpp
+++ main.cpp
@@ -1,33 +1,77 @@
 #include <bits/stdc++.h>
 
 #define ll long long
-    
 using namespace std;
-  
+
+int N, M;
+vector<int> pay;
+map<int,set<int>> friend_ship;
+
+void solve(void) {
+    vector<vector<int>> relations;
+    vector<bool> isVisisted(N+1, false);
+    queue<int> q;
+
+    for(int i = 1; i <= N; i++) {
+        if(isVisisted[i]) continue;
+        vector<int> temp_arr;
+        temp_arr.push_back(i);
+        q.push(i);
+        isVisisted[i] = true;
+        
+        while(!q.empty()) {
+            int temp = q.front();
+            q.pop();
+
+            for(auto f : friend_ship[temp]) {
+                if(!isVisisted[f]) {
+                    q.push(f);
+                    isVisisted[f] = true;
+                    temp_arr.push_back(f);
+                }
+            }
+        }
+
+        relations.push_back(temp_arr);
+    }
+
+    int count = 0;
+    for(auto rl : relations) {
+        int min_value = INT32_MAX;
+        for(auto _rl : rl) {
+            min_value = min(min_value, pay[_rl]);
+        }
+        count += min_value;
+    }
+    
+    cout << count << endl;
+}
+
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
+
 #ifdef HOANG_DEBUG
     freopen("input.txt", "r", stdin);
-#endif // HOANG_DEBUG
-    
-    int N;
-    cin >> N;
-    multiset<ll> cubes;
-    ll temp;
-    for(int i = 0; i < N; i++) {
-        cin >> temp;
-        auto it = cubes.upper_bound(temp);
-        if(it != cubes.end()) {
-            cubes.erase(it);
-        }
+#endif //HOANG_DEBUG
 
-        cubes.insert(temp);
+    cin >> N >> M;
+    pay.assign(N+1, 0);
+
+    for(int i = 1; i <= N; i++) {
+        cin >> pay[i];
+        friend_ship[i];
     }
 
-    cout << cubes.size() << endl;
+    int A, B;
+    for(int i = 0; i < M; i++) {
+        cin >> A >> B;
+        friend_ship[A].insert(B);
+        friend_ship[B].insert(A);
+    }
+
+    solve();
 
     return 0;
 }
