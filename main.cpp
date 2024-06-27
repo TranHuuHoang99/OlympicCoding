002d2a784637568bf732a613fedd48de508a436b
hoang2.tran <thhoang08091999@gmail.com>
2024-06-27 22:57:50 +0700

    competetive setup base

--- main.cpp
+++ main.cpp
@@ -3,27 +3,8 @@
 
 using namespace std;
 
-bool cmp(ll a, ll b) {
-    return a > b;
-}
+void solve(void) {
 
-void solve(const int N, vector<ll> B, vector<ll> C) {
-    sort(B.begin(), B.end());
-    sort(C.begin(), C.end(), cmp);
-
-    ll ret = abs(B[0]+C[0]);
-    int i = 0, j = 0;
-    while(i < N-1 || j < N-1) {
-        if(j >= N-1 || (i < N-1 && (B[i]+C[j] < 0))) {
-            i++;
-        } else {
-            j++;
-        }
-
-        ret = min(ret, abs(B[i]+C[j]));
-    }
-
-    cout << ret << endl;
 }
   
 int main(void) {
@@ -35,16 +16,7 @@ int main(void) {
     freopen("input.txt", "r", stdin);
 #endif // HOANG_DEBUG
 
-    int N;
-    cin >> N;
-    vector<ll> B(N);
-    vector<ll> C(N);
-
-    for(int i = 0; i < N; i++) cin >> B[i];
-
-    for(int i = 0; i < N; i++) cin >> C[i];
-
-    solve(N, B, C);
+    solve();
 
     return 0;
 }
