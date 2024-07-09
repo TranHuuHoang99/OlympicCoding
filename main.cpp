f6bd4a6725d374833d23c4b75abd733784418c0c
hoang2.tran <thhoang08091999@gmail.com>
2024-07-09 22:58:24 +0700

    [CODEFORCES] C. Increase and Copy

--- main.cpp
+++ main.cpp
@@ -3,30 +3,17 @@
 
 using namespace std;
 
+vector<ll> res;
+
 void solve(void) {
-    int N, H;
-    cin >> N >> H;
-    vector<vector<int>> woods(2);
-    for (int i = 0; i < N; i++) {
-        int temp;
-        cin >> temp;
-        woods[i%2].push_back(temp);
-    }
-
-    sort(woods[0].begin(), woods[0].end());
-    sort(woods[1].begin(), woods[1].end());
-
-    map<int,int> ret;
-
-    for (int i = 1; i <= H; i++) {
-        auto it_up = lower_bound(woods[0].begin(), woods[0].end(), i);
-        auto it_down = upper_bound(woods[1].begin(), woods[1].end(), H-i);
-        int len = N/2-(it_up - woods[0].begin()) + N/2-(it_down - woods[1].begin());
-        ret[len]++;
-    }
-
-    cout << ret.begin()->first << " " << ret.begin()->second << endl;
-}
+    ll N;
+    cin >> N;
+    const ll init = sqrt(N);
+    ll ret = init-1;
+    ret += ((N-init)/init);
+    if (N%init) ret++;
+    res.push_back(ret);
+}   
 
 int main(void) {
     ios_base::sync_with_stdio(false);
@@ -37,7 +24,15 @@ int main(void) {
     freopen("input.txt", "r", stdin);
 #endif // HOANG_DEBUG
 
-    solve();
+    int T;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    for (auto r : res) {
+        cout << r << endl;
+    }
 
     return 0;
 }
