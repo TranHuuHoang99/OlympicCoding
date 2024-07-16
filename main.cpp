115957b2c712635ea525426bc6ca9d83cc14c030
hoang2.tran <thhoang08091999@gmail.com>
2024-07-16 23:42:02 +0700

    [VNOI] Educational Backtracking: Biểu thức

--- main.cpp
+++ main.cpp
@@ -3,38 +3,53 @@
 
 using namespace std;
 
-int n;
-vector<int> A;
-const int alphabet = (1<<26)-1;
-int ret = 0;
+void dfs(int index, vector<ll> A, vector<int> S, int &ret, const ll &m) {
+    if (index >= A.size()) {
+        stack<pair<ll, int>> st;
+        st.push({A[0], 0});
 
-void dfs(int i, int val) {
-    if (val == alphabet) {
-        int temp = n-i;
-        ret += (1<<temp);
+        for (int i = 1; i < A.size(); i++) {
+            if (S[i] == 2) {
+                pair<ll,int> temp = st.top();
+                st.pop();
+                temp.first = (temp.first * A[i]) % m;
+                st.push(temp);
+            } else {
+                st.push({A[i], S[i]});
+            }
+        }
+
+        ll ret_temp = 0;
+        while (!st.empty()) {
+            pair<ll, int> temp = st.top();
+            st.pop();
+            if (temp.second == 0) {
+                ret_temp = (ret_temp + temp.first) % m;
+            } else {
+                ret_temp = (ret_temp - temp.first) % m;
+            }
+        }
+
+        if (ret_temp == 0) ret++;
         return;
     }
-    if (i >= n) {
-        if (val == alphabet) ret++;
-        return;
-    }
 
-    dfs(i+1, val);
-    dfs(i+1, val | A[i]);
+    for (int i = 0; i < 3; i++) {
+        S[index] = i;
+        dfs(index+1, A, S, ret, m);
+    }
 }
 
 void solve(void) {
-    cin >> n;
-    A.assign(n, 0);
-    string temp;
-    for (int i = 0; i < n; i++) {
-        cin >> temp;
-        for (auto t : temp) {
-            A[i] |= (1<<(t-'a'));
-        }
-    }
+    int n;
+    ll m;
+    cin >> n >> m;
+    vector<ll> A(n);
+    vector<int> S(n, 0);
+    for (int i = 0; i < n; i++) cin >> A[i];
 
-    dfs(0, 0);
+    int ret = 0;
+    dfs(1, A, S, ret, m);
 
     cout << ret << endl;
 }
@@ -48,7 +63,11 @@ int main(void) {
     freopen("input.txt", "r", stdin);
 #endif // HOANG_DEBUG
 
-    solve();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
 
     return 0;
 }
