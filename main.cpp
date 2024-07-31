0d22852261ca4dfc52772d1c5fd7bb2b70e6bafd
hoang2.tran <thhoang08091999@gmail.com>
2024-07-31 23:44:44 +0700

    [leetcode] Two Sum

--- main.cpp
+++ main.cpp
@@ -3,9 +3,20 @@
 
 using namespace std;
 
-void solve(void) {
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size()-1; i++) {
+            for (int j = i+1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i,j};
+                }
+            }
+        }
 
-}
+        return {-1,-1};
+    }
+};
   
 int main(void) {
     ios_base::sync_with_stdio(false);
@@ -16,7 +27,6 @@ int main(void) {
     freopen("input.txt", "r", stdin);
 #endif // HOANG_DEBUG
 
-    solve();
 
     return 0;
 }
