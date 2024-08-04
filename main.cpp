46e4b7246d927090bbb17f7b39abe0b6e6a6036b
hoang2.tran <thhoang08091999@gmail.com>
2024-08-04 21:38:47 +0700

    fix base setting

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
-#define ll long long
+#define int long long
 
 using namespace std;
 
 void solve(void) {
-
+    
 }
   
-int main(void) {
+int32_t main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
