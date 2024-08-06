7c5fe215ef1cdbc3c09e53433cc3c89d498dbc92
hoang2.tran <thhoang08091999@gmail.com>
2024-08-06 21:42:59 +0700

    [CSES] Range Update Queries

--- main.cpp
+++ main.cpp
@@ -1,59 +1,46 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define N 100000+1
+#define N 2*100000+1
 
 using namespace std;
 
-ll diff[N], A[N];
-int n, m;
+ll A[N], B[N];
+int n, q;
 
 void update(int index, ll val) {
-    for (int i = index; i <= n; i += i&-i) {
-        diff[i] += val;
-    }
+    for (int i = index; i <= n; i += i&-i) B[i] += val;
+}
+
+void updateRange(int u, int v, ll val) {
+    update(u, val);
+    update(v+1, -val);
 }
 
 ll sum(int index) {
     ll ret = 0;
-    for (int i = index; i > 0; i -= i&-i) ret += diff[i];
-    return ret;
-}
-
-int bs(ll target) {
-    int left = 1;
-    int right = n;
-    int ret = -1;
-    while (left <= right) {
-        int mid = (right+left) /2;
-        if (sum(mid) >= target) {
-            ret = mid;
-            right = mid-1;
-        } else {
-            left = mid + 1;
-        }
-    }
-
+    for (int i = index; i > 0; i -= i&-i) ret += B[i];
     return ret;
 }
 
 void solve(void) {
-    cin >> n;
-    for (int i = 1; i <= n; i++) cin >> A[i];
-    sort(A, A+n+1);
+    cin >> n >> q;
     for (int i = 1; i <= n; i++) {
-        update(i, A[i]-A[i-1]);   
+        cin >> A[i];
+        update(i, A[i]-A[i-1]);
     }
 
-    cin >> m;
-    while (m--) {
-        ll target;
-        cin >> target;
-        int i = bs(target);
-        if (i != -1) {
-            cout << n-i+1 << endl;
-            update(i, -1);
+    while (q--) {
+        int t;
+        cin >> t;
+        if (t == 1) {
+            int u, v;
+            ll x;
+            cin >> u >> v >> x;
+            updateRange(u, v, x);
         } else {
-            cout << 0 << '\n';
+            int u; 
+            cin >> u;
+            cout << sum(u) << endl;
         }
     }
 }
