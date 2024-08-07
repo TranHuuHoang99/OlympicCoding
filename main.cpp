f26e83405c8cc4e593c9c2f3a7e91ac099dd2c4e
hoang2.tran <thhoang08091999@gmail.com>
2024-08-07 22:58:39 +0700

    [CSES] Nested Ranges Count

--- main.cpp
+++ main.cpp
@@ -1,48 +1,66 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define N 2*100000+1
+#define N 10000000
 
 using namespace std;
 
-ll A[N], B[N];
-int n, q;
+int n;
+ll bit1[N], bit2[N];
+ll ans1[N], ans2[N];
 
-void update(int index, ll val) {
-    for (int i = index; i <= n; i += i&-i) B[i] += val;
+void update(ll* arr, ll index, int val) {
+    for (ll i = index; i <= n; i += i&-i) arr[i] += val;
 }
 
-void updateRange(int u, int v, ll val) {
-    update(u, val);
-    update(v+1, -val);
-}
-
-ll sum(int index) {
+ll sum(ll* arr, ll index) {
     ll ret = 0;
-    for (int i = index; i > 0; i -= i&-i) ret += B[i];
+    for (ll i = index; i > 0; i -= i&-i) ret += arr[i];
     return ret;
 }
 
-void solve(void) {
-    cin >> n >> q;
-    for (int i = 1; i <= n; i++) {
-        cin >> A[i];
-        update(i, A[i]-A[i-1]);
+bool cmp(pair<pair<ll,ll>, ll> A, pair<pair<ll,ll>, ll> B) {
+    if (A.first.first == B.first.first) {
+        return A.first.second > B.first.second;
     }
 
-    while (q--) {
-        int t;
-        cin >> t;
-        if (t == 1) {
-            int u, v;
-            ll x;
-            cin >> u >> v >> x;
-            updateRange(u, v, x);
-        } else {
-            int u; 
-            cin >> u;
-            cout << sum(u) << endl;
-        }
+    return A.first.first < B.first.first;
+}
+
+void solve(void) {
+    cin >> n;
+    set<ll> B;
+    set<ll> C;
+    map<ll,ll> m;
+    vector<pair<pair<ll,ll>, ll>> A(n);
+    for (int i = 0; i < n; i++) {
+        cin >> A[i].first.first >> A[i].first.second;
+        A[i].second = i;
+        B.insert(A[i].first.second);
     }
+
+    sort(A.begin(), A.end(), cmp);
+
+    int count = 0;
+    for (auto _B : B) {
+        m[_B] = ++count;
+    }
+
+    for (int i = n-1; i >= 0; i--) {
+        ans1[A[i].second] += sum(bit1, m[A[i].first.second]);
+        update(bit1, m[A[i].first.second], 1);
+    }
+
+    for (int i = 0; i < n; i++) cout << ans1[i] << ' ';
+    cout << '\n';
+
+    for (int i = 0; i < n; i++) {
+        ans2[A[i].second] += sum(bit2, m[A[i].first.second]);
+        update(bit2, 1, 1);
+        update(bit2, m[A[i].first.second]+1, -1);
+    }
+
+    for (int i = 0; i < n; i++) cout << ans2[i] << ' ';
+    cout << '\n';
 }
 
 int main(void) {
