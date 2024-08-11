b10d72331e463b82a8101ac5846f708af78fc46d
hoang2.tran <thhoang08091999@gmail.com>
2024-08-11 21:25:57 +0700

    add counting time complex

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 #define int long long
 
+#ifdef HOANG_DEBUG
+#include <time.h>
+#include <stdio.h>
+ 
+double time1, timedif;
+#endif // HOANG_DEBUG
+
 using namespace std;
 
 void solve(void) {
@@ -13,10 +20,18 @@ int32_t main(void) {
     cout.tie(NULL);
     
 #ifdef HOANG_DEBUG
-    freopen("input.txt", "r", stdin);
-#endif // HOANG_DEBUG
+	freopen("input.txt", "r", stdin);
+    time1 = (double)clock();
+    time1 = time1 / CLOCKS_PER_SEC; 
+#endif //HOANG_DEBUG
 
+    //write your code here
     solve();
+
+#ifdef HOANG_DEBUG
+    timedif = ( ((double) clock()) / CLOCKS_PER_SEC) - time1;
+    printf("The elapsed time is %lf seconds\n", timedif);
+#endif //HOANG_DEBUG
 
     return 0;
 }
