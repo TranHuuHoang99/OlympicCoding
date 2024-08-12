dde50336e1d72545e5b39e6ab2ec48b33f02ae04
hoang2.tran <thhoang08091999@gmail.com>
2024-08-12 22:38:21 +0700

    [LEETCODE] Count the Number of Consistent Strings

--- main.cpp
+++ main.cpp
@@ -5,15 +5,23 @@ using namespace std;
 
 class Solution {
 public:
-    int minBitFlips(int start, int goal) {
+    int countConsistentStrings(string allowed, vector<string>& words) {
         int ret = 0;
-        for (int i = 0; i < 32; i++) {
-            if ((start%2) != (goal%2)) {
-                ret++;
+        int mask = 0;
+        for (int i = 0 ; i < allowed.size(); i++) {
+            mask |= (1 << (allowed[i]-'a'));
+        }
+
+        for (int i = 0; i < words.size(); i++) {
+            bool isValid = true;
+            for (int j = 0; j < words[i].size(); j++) {
+                if ((mask >> (words[i][j]-'a')) % 2 == 0) {
+                    isValid = false;
+                    break;
+                }
             }
-            start >>= 1;
-            goal >>= 1;
-        }        
+            if (isValid) ret++;
+        }
 
         return ret;
     }
