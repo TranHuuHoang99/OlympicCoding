a97a4bf545b4255f5ad37532d4eb91cb35c00d0c
hoang2.tran <thhoang08091999@gmail.com>
2024-08-12 22:15:45 +0700

    [LEETCODE] Minimum Bit Flips to Convert Number

--- main.cpp
+++ main.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 class Solution {
 public:
-    int numberOfSteps(int num) {
+    int minBitFlips(int start, int goal) {
         int ret = 0;
-        while (num > 0) {
-            if (num % 2 == 0) {
-                num >>= 1;
-            } else {
-                num -= 1;
+        for (int i = 0; i < 32; i++) {
+            if ((start%2) != (goal%2)) {
+                ret++;
             }
-            ret++;
-        }
+            start >>= 1;
+            goal >>= 1;
+        }        
+
         return ret;
     }
 };
-  
+
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
