6e49c4a7756e4712bd4c9a30a0be534eca47969f
hoang2.tran <thhoang08091999@gmail.com>
2024-08-22 23:33:43 +0700

    [LEETCODE] Single Number

--- main.cpp
+++ main.cpp
@@ -5,24 +5,11 @@ using namespace std;
 
 class Solution {
 public:
-    int findKOr(vector<int>& nums, int k) {
+    int singleNumber(vector<int>& nums) {
         int ret = 0;
-        vector<int> bit(32, 0);
-
-        for (auto n : nums) {
-            int temp = n;
-            for (int i = 0; i < 32; i++) {
-                if (temp % 2 != 0) bit[i]++;
-                temp >>= 1;
-            }
+        for (int i = 0; i < nums.size(); i++) {
+            ret ^= nums[i];
         }
-        
-        for (int i = 0; i < 32; i++) {
-            if (bit[i] >= k) {
-                ret |= (1 << i);
-            }
-        }
-
         return ret;
     }
 };
