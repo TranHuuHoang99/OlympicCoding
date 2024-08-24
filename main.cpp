8ef12acb561e2feb5cc56d1fa3999b85b01edafb
hoang2.tran <thhoang08091999@gmail.com>
2024-08-24 19:50:50 +0700

    [LEETCODE] Number of 1 Bits

--- main.cpp
+++ main.cpp
@@ -1,22 +1,17 @@
 #include <bits/stdc++.h>
+#include <chrono>
+#include <thread>
 #define ll long long
 
 using namespace std;
 
 class Solution {
 public:
-    vector<int> evenOddBit(int n) {
-        vector<int> ret(2,0);
+    int hammingWeight(int n) {
+        int ret = 0;
         for (int i = 0; i < 32; i++) {
-            if (n & (1 << i)) {
-                if (i % 2 == 0) {
-                    ret[0]++;
-                } else {
-                    ret[1]++;
-                }
-            }
+            if (n & (1 << i)) ret++;
         }
-
         return ret;
     }
 };
@@ -29,10 +24,6 @@ int main(void) {
 #ifdef HOANG_DEBUG
     freopen("input.txt", "r", stdin);
 #endif // HOANG_DEBUG
-
-
-    char temp = '\0';
-    cout << temp << endl;
 
     return 0;
 }
