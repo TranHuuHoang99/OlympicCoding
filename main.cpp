1af225fc3762c8a6c7608c4bd8123256fad346eb
hoang2.tran <thhoang08091999@gmail.com>
2024-08-26 09:21:27 +0700

    [LEETCODE] Count Complete Tree Nodes

--- main.cpp
+++ main.cpp
@@ -5,18 +5,36 @@
 
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        vector<bool> v(nums.size()+1, false);
-        for (auto n : nums) {
-            v[n] = true;
+    int countNodes(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int ret = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* temp = q.front();
+            q.pop();
+            ret++;
+            if (temp->left != nullptr) {
+                q.push(temp->left);
+            }
+
+            if (temp->right != nullptr) {
+                q.push(temp->right);
+            }
         }
 
-        int ret = 0;
-        for (int i = 0; i < v.size(); i++) {
-            if (!v[i]) ret = i;
-        } 
         return ret;
     }
 };
