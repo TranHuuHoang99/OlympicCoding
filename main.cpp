5af80a952e642965d6ecc86e310bd545a4c4f33d
hoang2.tran <thhoang08091999@gmail.com>
2024-08-26 13:48:53 +0700

    [LEETCODE] Binary Watch

--- main.cpp
+++ main.cpp
@@ -7,20 +7,51 @@ using namespace std;
 
 class Solution {
 public:
-    char findTheDifference(string s, string t) {
-        char ret = 0;
-        int left = 0;
-        int right = 0;
-        for (char c : s) left ^= (1 << (c-'a'));
-        for (char c : t) right ^= (1 << (c-'a'));
-        
-        int ret_mask = left ^ right;
-        for (int i = 0; i <= 26; i++) {
-            int mask = 1 << i;
-            if (mask & ret_mask) {
-                ret = 'a'+i;
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> ret;
+        if (turnedOn == 0) {
+            string str = "0:00";
+            ret.push_back(str);
+            return ret;
+        }
+        const int len = 1 << 10;
+        for (int mask = 1; mask < len; mask++) {
+            int cnt = 0;
+            for (int i = 0; i < 10; i++) {
+                if (mask & (1 << i)) cnt++;
             }
-        }   
+
+            if (cnt == turnedOn) {
+                int hour = 0;
+                for (int i = 0; i <= 3; i++) {
+                    if (mask & (1 << i)) {
+                        hour += (1 << i);
+                    }
+                }   
+
+                int minute = 0;
+                for (int i = 4; i <= 9; i++) {
+                    if (mask & (1 << i)) {
+                        minute += (1 << (i-4));
+                    }
+                }
+
+                if (hour <= 11 && minute <= 59) {
+                    string str;
+                    str += to_string(hour);
+                    str += ':';
+                    if (minute < 10) {
+                        str += '0';
+                        str += to_string(minute);
+                    } else {
+                        str += to_string(minute);
+                    }   
+
+                    ret.push_back(str);
+                }
+            }
+        }
+
         return ret;
     }
 };
