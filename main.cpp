62894530c30cd061b902fd0dc8f4ad3114963aa9
hoang2.tran <thhoang08091999@gmail.com>
2024-09-03 22:52:05 +0700

    [CSES][DP] Two Sets II

--- main.cpp
+++ main.cpp
@@ -10,28 +10,45 @@ double time1, timedif;
 
 using namespace std;
 
-const ll N = 1e3*5 + 1;
-ll x[N];
+const ll M = 1e5 * 2;
+const ll N = 1e2 * 5 + 1;
+const ll MOD = 1e9+7;
 int n;
-vector<vector<ll>> dp(N, vector<ll>(N, -1));
+ll dp[N][M];
 
-ll dfs(int i, int j) {
-    if (i == j || dp[i][j] != -1) return dp[i][j];
-    
-    ll temp = max(x[i] - dfs(i+1, j), x[j] - dfs(i, j-1));
-    return dp[i][j] = temp;
+ll bin(const ll a, ll b) {
+    if (b == 0) return 1;
+    ll temp = bin(a, b >> 1);
+    temp *= temp;
+    temp %= MOD;
+    if (b & 1) return (temp * a) % MOD;
+    return temp;
 }
 
 void solve(void) {
     cin >> n;
-    ll sum = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> x[i];
-        dp[i][i] = x[i];
-        sum += x[i];
+    int sum = 0;
+    for (int i = 1; i <= n; i++) sum += i;
+    if (sum % 2 != 0) {
+        cout << 0 << endl;
+        return;
     }
-   
-    cout << (dfs(0, n-1) + sum) / 2 << endl;
+
+    sum /= 2;
+    for (int i = 0; i <= n; i++) dp[i][0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = sum; j >= 1; j--) {
+            dp[i][j] += dp[i-1][j];
+            dp[i][j] %= MOD;
+            if (j - i >= 0) {
+                dp[i][j] += dp[i-1][j-i];
+                dp[i][j] %= MOD;
+            }
+        }
+    }
+
+    cout << (dp[n][sum] * bin(2, MOD-2)) % MOD << endl;
 }
   
 int main(void) {
