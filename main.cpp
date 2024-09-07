e4ad51695da723b557e1258c2d6486fd606f5bba
hoang2.tran <thhoang08091999@gmail.com>
2024-09-08 01:32:09 +0700

    [CSES][DP] Elevator Rides

--- main.cpp
+++ main.cpp
@@ -3,54 +3,34 @@
 
 using namespace std;
 
-struct Node {
-	ll x, y, val;
-};
-
-const ll N = 1e5 * 2 + 1;
-Node A[N];
-ll dp[N];
-ll pr[N];
+const ll N = 20+1;
+const ll M = 1 << 20;
 int n;
-
-bool cmp(Node a, Node b) {
-	if (a.y == b.y) return a.x < b.x;
-	return a.y < b.y;
-}
+ll x;
+ll w[N];
 
 void solve(void) {
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> A[i].x >> A[i].y >> A[i].val;
+	cin >> n >> x;
+	for (int i = 0; i < n; i++) cin >> w[i];
+	vector<pair<ll,ll>> dp((1 << n) + 1, {LLONG_MAX, LLONG_MAX});
+	dp[0] = {1,0};
+
+	for (int i = 1; i < (1 << n); i++) {
+		for (int j = 0; j < n; j++) {
+			if (i & (1 << j)) {
+				int mask = i ^ (1 << j);
+				if (dp[mask].second + w[j] <= x) {
+					dp[i] = {dp[mask].first, min(dp[i].second, dp[mask].second + w[j])};
+				} else {
+					if (dp[mask].first + 1 <= dp[i].first) {
+						dp[i] = {dp[mask].first + 1, min(dp[i].second, w[j])};
+					}
+				}
+			}		
+		}	
 	}
 
-	sort(A, A+n, cmp);
-
-	for (int i = 0; i < n; i++) {
-		ll left = 0;
-		ll right = i-1;
-		ll idx = -1;
-		
-		while (left <= right) {
-			ll mid = (left + right) >> 1;
-			if (A[mid].y < A[i].x) {
-				idx = mid;
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
-
-		if (idx != -1) {
-			dp[i] = pr[idx] + A[i].val;
-			pr[i] = max(pr[i-1], dp[i]);
-		} else {
-			dp[i] = A[i].val;
-			pr[i] = max(pr[i-1], dp[i]);
-		}
-	}
-
-	cout << *max_element(dp, dp+n) << endl;
+	cout << dp[(1 << n) - 1].first << endl;
 }
   
 int main(void) {
