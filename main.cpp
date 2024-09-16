c0939020f7f4ce7df621d9fdcfe1ba689586d0eb
hoang2.tran <thhoang08091999@gmail.com>
2024-09-17 00:30:01 +0700

    [LEETCODE] Maximal Rectangle

--- main.cpp
+++ main.cpp
@@ -1,36 +1,62 @@
-#include <bits/stdc++.h>
-#include <chrono>
-#include <thread>
-#define ll long long
-
-using namespace std;
-
 class Solution {
 public:
-    vector<int> findErrorNums(vector<int>& nums) {
-        unordered_set<int> unqNums;
-        vector<int> res;
-        for(auto num : nums){
-            if(unqNums.count(num)){
-                res.push_back(num);
-            }
-            unqNums.insert(num);
-        }
-        for(int num = 1; num <= nums.size(); num++){
-            if(!unqNums.count(num))res.push_back(num);
-        }
-        return res;
-    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+		int ret = 0;
+		vector<vector<int>> dp(2, vector<int>(matrix[0].size(), 0));
+
+		for (int i = 0; i < matrix.size(); i++) {
+			for (int j = 0; j < matrix[i].size(); j++) {
+				if (matrix[i][j] == '0') {
+					dp[1][j] = 0;
+				} else {
+					dp[1][j] = dp[0][j] + 1;
+				}
+			}
+
+			stack<int> hs;
+			stack<int> ps;
+		
+			for (int j = 0; j < dp[1].size(); j++) {
+				if (hs.empty() || hs.top() < dp[1][j]) {
+					hs.push(dp[1][j]);
+					ps.push(j);
+					continue;
+				}
+				
+				int temp_pos = -1;
+				while (!hs.empty() && hs.top() > dp[1][j]) {
+					int height = hs.top();
+					hs.pop();
+
+					int width = j - ps.top();
+					temp_pos = ps.top();
+					ps.pop();
+
+					ret = max(ret, height * width);
+				}
+
+				if (temp_pos != -1) {
+					hs.push(dp[1][j]);
+					ps.push(temp_pos);
+				}
+			}
+
+			cout << i << ' ' << hs.size() << ' ' << ps.size() << endl;	
+
+			const int len = dp[1].size();
+			while (!hs.empty()) {
+				int height = hs.top();
+				hs.pop();
+				int width = len - ps.top();
+				ps.pop();
+
+				ret = max(ret, height * width);
+			}
+
+			swap(dp[1], dp[0]);
+		}
+
+		return ret;
+	}
 };
 
-int main(void) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-#ifdef HOANG_DEBUG
-    freopen("input.txt", "r", stdin);
-#endif // HOANG_DEBUG
-
-    return 0;
-}
