c5731b700cfd9fae6f7c2c1d183b77ca130a7802
hoang2.tran <thhoang08091999@gmail.com>
2024-09-19 22:23:28 +0700

    [CSES][GRAPH] Building Teams

--- main.cpp
+++ main.cpp
@@ -3,11 +3,10 @@
 
 using namespace std;
 
-const ll N = 1e5 + 1;
-vector<pair<int,int>> graph(N, {0, INT32_MAX});
-bool v[N];
+const ll N = 1e5+1;
+int ret[N];
 int n, m;
-map<int, set<int>> mp;
+map<int,set<int>> mp;
 
 void solve(void) {
 	cin >> n >> m;
@@ -18,53 +17,32 @@ void solve(void) {
 		mp[b].insert(a);
 	}
 
-	queue<int> q;
-	graph[1] = {1,1};
+	queue<int> q;	
 	for (int i = 1; i <= n; i++) {
-		if (v[i] || graph[i].second == INT32_MAX) continue;
+		if (ret[i] != 0) continue;
+		ret[i] = 1;
 		q.push(i);
-		v[i] = true;
 
 		while (!q.empty()) {
 			int temp = q.front();
 			q.pop();
-			
+
 			for (auto s : mp[temp]) {
-				if (graph[s].second > graph[temp].second + 1) {
-					graph[s].second = graph[temp].second + 1;
-					graph[s].first = temp;
+				if (ret[s] != 0 && ret[s] == ret[temp]) {
+					cout << "IMPOSSIBLE" << endl;
+					return;
 				}
 
-				if (!v[s]) {
-					v[s] = true;
+				if (ret[s] == 0) {
+					ret[s] = ret[temp] == 1 ? 2 : 1;
 					q.push(s);
 				}
 			}
 		}
-	}
+	}	
 
-	if (graph[n].second == INT32_MAX) {
-		cout << "IMPOSSIBLE" << endl;
-	} else {
-		stack<int> ret;
-		queue<int> n_q;
-		n_q.push(n);
-		
-		while (!n_q.empty()) {
-			int temp = n_q.front();
-			n_q.pop();
-			ret.push(temp);
-			if (temp == 1) break;
-			n_q.push(graph[temp].first);	
-		}
-
-		cout << graph[n].second << endl;
-		while (!ret.empty()) {
-			cout << ret.top() << ' ';
-			ret.pop();
-		}
-		cout << '\n';
-	}
+	for (int i = 1; i <= n; i++) cout << ret[i] << ' ';
+   	cout << '\n';	
 }
   
 int main(void) {
