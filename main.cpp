0d18f5900688332b1b0a2f6679c3d5c9a8623e61
hoang2.tran <thhoang08091999@gmail.com>
2024-10-02 22:18:44 +0700

    [CSES][GRAPH] High Score

--- main.cpp
+++ main.cpp
@@ -3,40 +3,75 @@
 
 using namespace std;
  
-const ll N = 500 + 1;
-int n, m, q;
+const ll N = 2500;
+bool v_s[N];
+bool v_e[N];
+vector<int> btoe[N];
+vector<int> etob[N];
+int n, m;
+
+void bfs(vector<int> arr[], int a, bool v[]) {	
+	queue<int> q;
+	q.push(a);
+	v[a] = true;
+	while (!q.empty()) {
+		int temp = q.front();
+		q.pop();
+		for (int next : arr[temp]) {
+			if (!v[next]) {
+				q.push(next);
+				v[next] = true;
+			}
+		}
+	}
+}
 
 void solve(void) {
-	cin >> n >> m >> q;
-	vector<vector<ll>> update(n+1, vector<ll>(n+1, LLONG_MAX));
+	cin >> n >> m;
+	vector<tuple<int,int,ll>> tp(m);
 	for (int i = 0; i < m; i++) {
 		int a, b;
 		ll c;
 		cin >> a >> b >> c;
-		update[a][b] = min(update[a][b], c);
-		update[b][a] = min(update[b][a], c);
+		--a; --b;
+		tp[i] = {a,b,-c};
+		btoe[a].push_back(b);
+		etob[b].push_back(a);
 	}
 
-	for (int i = 1; i <= n; i++) update[i][i] = 0;
+	bfs(btoe, 0, v_s);
+	bfs(etob, n-1, v_e);
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			for (int k = 1; k <= n; k++) {
-				if (update[j][i] == LLONG_MAX || update[i][k] == LLONG_MAX) continue;
-				update[j][k] = min(update[j][k], update[j][i] + update[i][k]);
-			}
+	vector<ll> update(n, LLONG_MAX);
+	update[0] = 0;
+	// bellman-ford
+	for (int i = 0; i < n-1; i++) {
+		for (int j = 0; j < m; j++) {
+			int a, b;
+			ll c;
+			tie(a,b,c) = tp[j];
+			if (update[a] == LLONG_MAX) continue;
+			update[b] = min(update[b], update[a] + c);
 		}
 	}
-	
-	while (q--) {
+
+	vector<ll> temp_u = update;
+	for (int i = 0; i < m; i++) {
 		int a, b;
-		cin >> a >> b;
-		if (update[a][b] == LLONG_MAX) {
+		ll c;
+		tie(a, b, c) = tp[i];
+		if (update[a] == LLONG_MAX) continue;
+		update[b] = min(update[b], update[a] + c);
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (update[i] != temp_u[i] && v_s[i] && v_e[i]) {
 			cout << -1 << endl;
-		} else {
-			cout << update[a][b] << endl;
+			return;
 		}
-	}		
+	}
+
+	cout << -update[n-1] << endl;
 }
 
 int main(void) {
@@ -48,3 +83,23 @@ int main(void) {
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
