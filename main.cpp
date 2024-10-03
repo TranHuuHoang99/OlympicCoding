af3f72bad2d8b73c670c64483684884de5300dbe
hoang2.tran <thhoang08091999@gmail.com>
2024-10-03 22:30:59 +0700

    [LEETCODE][SORT] 3Sum

--- main.cpp
+++ main.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    int calculate(string s) {
- 		int ret = 0;
-		
-		stack<int> st;
-		int numb = 0;
-		int n_sign = 1;
-		for (char c : s) {
-			if (isdigit(c)) {
-				numb = numb * 10 + static_cast<int>(c-'0');
-			} else if (c == '+' || c == '-') {
-				ret += numb * n_sign;
-				n_sign = c == '+' ? 1 : -1;
-				numb = 0;
-			} else if (c == '(') {
-				st.push(ret);
-				st.push(n_sign);
-				ret = 0;
-				n_sign = 1;
-			} else if (c == ')') {
-				ret += numb * n_sign;
-				ret *= st.top();
-				st.pop();
-				ret += st.top();
-				st.pop();
-				numb = 0;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+		vector<vector<int>> ret;
+		sort(nums.begin(), nums.end());
+
+		for (int i = 0; i < nums.size() - 2; i++) {
+			for (int j = i+1; j < nums.size() - 1; j++) {
+				int left = j + 1;
+				int right = nums.size() - 1;
+				while (left <= right) {
+					int mid = left + (right - left) / 2;
+					if (nums[mid] == -(nums[i] + nums[j])) {
+						ret.push_back({nums[mid], nums[i], nums[j]});
+						break;
+					} else if (nums[mid] < -(nums[i] + nums[j])) {
+						left = mid + 1;
+					} else {
+						right = mid - 1;
+					}
+				}
+
+				while (j+1 < nums.size()-1 && nums[j] == nums[j+1]) j++;
 			}
-		}		
 
-		return ret + numb * n_sign;
-    }
+			while (i+1 < nums.size() && nums[i] == nums[i+1]) i++;
+		}
+			
+		return ret;		
+	}
 };
-
