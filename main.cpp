3a5fb4f200b63beb21d931a8ed695684fcb03cc3
hoang2.tran <thhoang08091999@gmail.com>
2024-10-05 22:11:18 +0700

    [SWPCT][BATCH4] Manig-En

--- main.cpp
+++ main.cpp
@@ -3,52 +3,43 @@
 #define ll long long
 using namespace std;
 
-const ll N = 1e5+1;
-ll arr1[N], arr2[N], dp1[N], dp2[N];
-int N1, N2;
-
 void solve(void) {
-	cin >> N1;
-	for (int i = 1; i <= N1; i++) cin >> arr1[i];
-	cin >> N2;
-	for (int i = 1; i <= N2; i++) cin >> arr2[i];
-
-	dp1[1] = arr1[1];
-	dp2[1] = arr2[1];
-
-	for (int i = 2; i <= N1; i++) dp1[i] = dp1[i-1] + arr1[i];
-	for (int i = 2; i <= N2; i++) dp2[i] = dp2[i-1] + arr2[i];
-
-	map<ll,int> m;
-	for (int i = 1; i <= N2; i++) m[arr2[i]] = i;
-
-	int first = 0;
-	int second = 0;
+	string str;
+	int k;
+	cin >> str >> k;
 	
-	vector<ll> ret_arr;
-	for (int i = 1; i <= N1; i++) {
-		if (m.find(arr1[i]) != m.end()) {
-			int index = m[arr1[i]];
-			ll temp = max(dp2[index] - dp2[second], dp1[i] - dp1[first]);
-			first = i;
-			second = index;
-			ret_arr.push_back(temp);
+	string ret;
+	string sub_str;
+	int seq = 1;
+	for (int i = 0; i < str.size(); i++) {
+		if (sub_str.size() < k) {
+			sub_str += str[i];
+		} else {
+			string temp;
+			for (int j = 0; j < sub_str.size(); j++) {
+				char c = sub_str[j] - 'a';
+				c += (26 + k * seq);
+				c %= 26;
+				temp += c + 'a';
+			}
+			ret += temp;
+			sub_str.clear();
+			sub_str += str[i];
+			seq *= -1;
 		}
-		if (first == N1 || second == N2) break; 
 	}
 
-	ll ret = 0;
-	for (ll r : ret_arr) ret += r;
-
-	if (first < N1 && second < N2) {
-		ll temp = max(dp1[N1] - dp1[first], dp2[N2] - dp2[second]);
-		ret += temp;
-	} else if (first < N1 && second == N2) {
-		ret += dp1[N1] - dp1[first];
-	} else if (first == N1 && second < N2) {
-		ret += dp2[N2] - dp2[second];
+	if (!sub_str.empty() && sub_str.size() < k) {
+		ret += sub_str;
 	} else {
-		ret = ret;
+		string temp;
+		for (int i = 0; i < sub_str.size(); i++) {
+			char c = sub_str[i] - 'a';
+			c += 26 + k * seq;
+			c %= 26;
+			temp += c + 'a';
+		}
+		ret += temp;
 	}
 
 	cout << ret << endl;
