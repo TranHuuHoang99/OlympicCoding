934a5e75f2ce442889dd4acfa77c44f8fcb71b4b
hoang2.tran <thhoang08091999@gmail.com>
2024-10-07 00:27:39 +0700

    [CSES][GRAPH] Cycle Finding

--- main.cpp
+++ main.cpp
@@ -3,60 +3,70 @@
 
 using namespace std;
 
-const ll N = 1e5+1;
-const ll M = N * 2;
-vector<pair<ll,int>> path1[N];
-vector<pair<ll,int>> path2[N];
-tuple<int,int,ll> tp[M];
-int n,m;
-
-void bfs(vector<ll>& dis, int start, vector<pair<ll, int>> path[]) {
-	priority_queue<pair<ll,int>, vector<pair<ll,int>>, greater<pair<ll,int>>> q;
-	dis[start] = 0;
-	q.push({0,start});
-	while (!q.empty()) {
-		auto [x,y] = q.top();
-		q.pop();
-		
-		if (x > dis[y]) continue;
-		for (pair<ll,int> p : path[y]) {
-			if (dis[p.second] > p.first + dis[y]) {
-				dis[p.second] = p.first + dis[y];
-				q.push({dis[p.second], p.second});	
-			}
-		}
-	}
-}
-
+const ll N = 5000+1;
+int n,m,a,b;
+ll c;
+tuple<int,int,ll> tp[N];
 
 void solve(void) {
 	cin >> n >> m;
-	vector<ll> dis1(n, LLONG_MAX);
-	vector<ll> dis2(n, LLONG_MAX);	
-
 	for (int i = 0; i < m; i++) {
-		int a,b;
-		ll c;
 		cin >> a >> b >> c;
 		--a; --b;
-		path1[a].push_back({c,b});
-		path2[b].push_back({c,a});
 		tp[i] = {a,b,c};
 	}
-	
-	bfs(dis1, 0, path1);
-	bfs(dis2, n-1, path2);
 
-	ll ret = LLONG_MAX;
-	for (int i = 0; i < m; i++) {
-		int a, b;
-		ll c;
-		tie(a, b, c) = tp[i];
-		if (dis1[a] == LLONG_MAX || dis2[b] == LLONG_MAX) continue;
-		ret = min(ret, dis1[a] + dis2[b] + c/2);
+	vector<ll> update(n, 1e18);
+	update[0] = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			tie(a,b,c) = tp[j];
+			update[b] = min(update[b], update[a] + c);
+		}
 	}
 
-	cout << ret << endl;
+	bool isValid = false;
+	for (int i = 0; i < m; i++) {
+		tie(a,b,c) = tp[i];
+		if (update[b] > update[a] + c) {
+			isValid = true;
+			break;
+		}
+	}
+
+	if (!isValid) {
+		cout << "NO" << endl;
+		return;
+	}
+
+	cout << "YES" << endl;
+	int start = -1;
+	vector<int> change(n, -1);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			tie(a,b,c) = tp[j];
+			if (update[b] > update[a] + c) {
+				update[b] = update[a] + c;
+				change[b] = a;
+				start = b;
+			}
+		}
+	}
+
+	for (int i = 0; i < change.size(); i++) start = change[start];
+	int cur = start;
+	stack<int> st;
+	do {
+		st.push(cur);
+		cur = change[cur];
+	} while (cur != start);
+
+	st.push(cur);
+	while (!st.empty()) {
+		cout << st.top() + 1 << ' ';
+		st.pop();
+	}
+	cout << endl;
 }
 
 int main(void) {
@@ -68,23 +78,3 @@ int main(void) {
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
