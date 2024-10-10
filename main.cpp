a700019816a82848bdf2940a063890635026abe7
hoang2.tran <thhoang08091999@gmail.com>
2024-10-11 00:24:58 +0700

    [CSES][GRAPH] Course Schedule

--- main.cpp
+++ main.cpp
@@ -4,73 +4,40 @@
 using namespace std;
 
 const ll N = 1e5+1;
-set<int> path[N];
-bool v[N];
-bool rs_v[N];
-stack<int> st;
+vector<int> ret;
+vector<int> path[N];
+int arr[N];
 int n, m;
-
-bool dfs(int cur) {
-	v[cur] = true;
-	rs_v[cur] = true;
-	st.push(cur);
-	for (int next : path[cur]) {
-		if (!v[next]) {
-			if (dfs(next)) return true;
-		} 
-		if (rs_v[next]) {
-			st.push(next);
-			return true;
-		}
-	}
-
-	rs_v[cur] = false;
-	st.pop();
-	return false;
-}
 
 void solve(void) {
 	cin >> n >> m;
 	for (int i = 0; i < m; i++) {
-		int a,b;
+		int a, b;
 		cin >> a >> b;
-		path[a].insert(b);
+		path[a].push_back(b);
+		arr[b]++;
 	}
-	
-	for (int i = 1; i <= n; i++) {
-		if (!v[i] && dfs(i)) {
-			vector<int> ret;
-			int temp = st.top();
-			st.pop();
-			while (!st.empty()) {
-				ret.push_back(st.top());
-				st.pop();
-				if (temp == ret.back() && st.size() != 1) break;
-			}
 
-			reverse(ret.begin(), ret.end());
-			ret.push_back(temp);
+	queue<int> q;
+	for (int i = 1; i <= n; i++) if (!arr[i]) q.push(i);
 
-			int idx = 0;
-			for (int i = 0; i < ret.size(); i++) {
-				if (ret[i] == temp) {
-					idx = i;
-					break;
-				}
-			}
-
-			if (idx != 0) {
-				idx--;
-				ret.erase(ret.begin()+idx);
-			}
-			cout << ret.size() << endl;		
-			for (int r : ret) cout << r << ' ';
-			cout << endl;
-			return;
+	while (!q.empty()) {
+		int temp = q.front();
+		q.pop();
+		ret.push_back(temp);
+		
+		for (int next : path[temp]) {
+			arr[next]--;
+			if (!arr[next]) q.push(next);
 		}
 	}
 
-	cout << "IMPOSSIBLE" << endl;
+	if (ret.size() != n) {
+		cout << "IMPOSSIBLE" << endl;
+	} else {
+		for (int i = 0; i < ret.size(); i++) cout << ret[i] << ' ';
+		cout << endl;
+	}
 }
 
 int main(void) {
