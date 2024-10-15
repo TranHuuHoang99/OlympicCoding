0d78d8434e858370533940eba4c3674abea30c2d
hoang2.tran <thhoang08091999@gmail.com>
2024-10-15 20:26:34 +0700

    update environment

--- main.cpp
+++ main.cpp
@@ -1,37 +1,14 @@
 #include <bits/stdc++.h>
 #define int long long
 
-#ifdef HOANG_DEBUG
-#include <time.h>
-#include <stdio.h>
- 
-double time1, timedif;
-#endif // HOANG_DEBUG
-
 using namespace std;
 
-void solve(void) {
-    
-}
-  
-int32_t main(void) {
+int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
-#ifdef HOANG_DEBUG
-	freopen("input.txt", "r", stdin);
-    time1 = (double)clock();
-    time1 = time1 / CLOCKS_PER_SEC; 
-#endif //HOANG_DEBUG
 
-    //write your code here
-    solve();
-
-#ifdef HOANG_DEBUG
-    timedif = ( ((double) clock()) / CLOCKS_PER_SEC) - time1;
-    printf("The elapsed time is %lf seconds\n", timedif);
-#endif //HOANG_DEBUG
+	
 
     return 0;
 }
