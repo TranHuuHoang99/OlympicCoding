92256d28858694824eb6ceeb3cee2d8820d66fe3
hoang2.tran <thhoang08091999@gmail.com>
2024-11-06 23:53:00 +0700

    autumn contest 2019

---
+++ autumn_contest_2019/AC1.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+
+void solve(void) {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+}
+
+void rwFile(string input, string output) {
+	freopen(input.c_str(), "r", stdin);
+	freopen(output.c_str(), "w", stdout);
+}
+
+int32_t main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+	rwFile("AC1.inp", "AC1.out");
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	
+    return 0;
+}
+
