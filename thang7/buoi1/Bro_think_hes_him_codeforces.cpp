0f523f797cb54997a25b36f361221e922055d8c8
hoangprodn <thhoang08091999@gmail.com>
2025-06-08 22:07:40 +0700

    update form bai tap thang 7 buoi 1

---
+++ thang7/buoi1/Bro_think_hes_him_codeforces.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+void solve(void) {
+	
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	solve();
+
+	return 0;
+}
+
