9144428aba3b28822bda559a2cb2ac62972bce2f
hoangprodn <thhoang08091999@gmail.com>
2025-06-09 22:06:28 +0700

    thang 6 buoi 6

--- thang6/buoi6/vnoicup24_r2_c_vnoi.cpp
+++ thang6/buoi6/vnoicup24_r2_c_vnoi.cpp
@@ -4,20 +4,48 @@
 using namespace std;
 
 const ll N = 3e5+10;
-ll A[N], C[N];
-ll sumA[N];
-ll sumC[N];
+pair<ll,ll> AC[N];
+ll prefixC[N], suffixC[N], prefixAC[N], suffixAC[N];
 int n;
+
+bool cmp(pair<ll,ll> a, pair<ll,ll> b) {
+ 	return a.first < b.first;
+}
 
 void solve(void) {
 	cin >> n;
-	for (int i = 1; i <= n; i++) cin >> A[i];
-	for (int i = 1; i <= n; i++) cin >> C[i];
-	for (int i = 1; i <= n; i++) {
-	 	sumA[i] = sumA[i-1] + A[i];
-	 	sumC[i] = sumC[i-1] + C[i];
+	for (int i = 0; i <= n+3; i++) {
+	 	AC[i] = {0ll,0ll};
+	 	prefixC[i] = 0ll;
+	 	suffixC[i] = 0ll;
+	 	prefixAC[i] = 0ll;
+	 	suffixAC[i] = 0ll;
 	}
-
+	for (int i = 1; i <= n; i++) cin >> AC[i].first;
+	for (int i = 1; i <= n; i++) cin >> AC[i].second;
+	sort(AC+1, AC+1+n, cmp);
+	for (int i = 1; i <= n; i++) {
+	 	prefixC[i] = prefixC[i-1] + AC[i].second;
+	 	prefixAC[i] = prefixAC[i-1] + AC[i].first * AC[i].second;
+	}
+	for (int i = n; i >= 1; i--) {
+	 	suffixC[i] = suffixC[i+1] + AC[i].second;
+	 	suffixAC[i] = suffixAC[i+1] + AC[i].first * AC[i].second;
+	}
+	if (prefixC[n] < 0) {
+	 	cout << "-inf" << endl;
+	 	return;
+	}
+	ll ret = LLONG_MAX;
+	for (int i = 0; i <= n; i++) {
+	 	ll temp = prefixC[i] - suffixC[i+1];
+	 	if (temp <= 0) {
+	 	 	ret = min(ret, temp * AC[i+1].first - prefixAC[i] + suffixAC[i+1]);
+	 	} else {
+	 	 	ret = min(ret, temp * AC[i].first - prefixAC[i] + suffixAC[i+1]);
+	 	}
+	}
+	cout << ret << endl;
 }
 
 int main(void) {
--- thang6/buoi6/vnoicup24_r2_e_vnoi.cpp
+++ thang6/buoi6/vnoicup24_r2_e_vnoi.cpp
@@ -1,28 +1,110 @@
-#include <bits/stdc++.h>
-#define ll long long
-
-using namespace std;
-
-void solve(void) {
-	
+#include <iostream>
+#include <algorithm>
+#include <vector> 
+#include <math.h>
+struct edge {
+  int u, v;
+  long long weight; 
+  bool operator < (const edge &other) {
+    return weight < other.weight; 
+  }
+};
+int numTest;
+int n, m, k;  
+int root[1005];
+std::vector <edge> listEdges[10];
+std::vector <edge> listImportantEdges[10];
+std::vector <std::vector <int> > listStates;
+long long s[10];   
+int findRoot(int u) {
+  return root[u] = (u == root[u] ? u : findRoot(root[u])); 
 }
-
-int main(void) {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-#ifdef HOANGPRODN_DEBUG
-	freopen("input.in", "r", stdin);
-	freopen("output.out", "w", stdout);
-#endif // HOANGPRODN_DEBUG
-
-	int t;
-	cin >> t;
-	for (int i = 1; i <= t; i++) {
-	 	solve();
-	}
-
-	return 0;
+void solve(const int &net) {
+  for (int i = 1; i <= n; i++) {
+    root[i] = i; 
+  }
+  std::vector <edge> &e = listEdges[net]; 
+  std::sort(e.begin(), e.end()); 
+  long long cost = 0LL; 
+  for (int i = 0; i < (int) e.size(); i++) {
+    int u = e[i].u, v = e[i].v; 
+    int rootu = findRoot(u);
+    int rootv = findRoot(v); 
+    if (rootu != rootv) {
+      cost += e[i].weight; 
+      listImportantEdges[net].push_back(e[i]); 
+      root[rootu] = rootv; 
+    }
+  }                              
+}                        
+void backTrack(int pos, std::vector <int> curState) {
+  if (pos == k) {
+    listStates.push_back(curState); 
+    return; 
+  }  
+  for (int choose = 0; choose <= 1; choose++) {
+    std::vector <int> tmp = curState;
+    tmp.push_back(choose); 
+    backTrack(pos + 1, tmp); 
+  }
 }
-
+int main () {
+  std::cin >> numTest;
+  for (int testCase = 1; testCase <= numTest; testCase++) {
+    std::cin >> n >> m >> k; 
+    listStates.clear(); 
+    backTrack(0, {}); 
+    for (int net = 0; net < k; net++) {
+      listEdges[net].clear();
+      listImportantEdges[net].clear(); 
+    }
+    for (int i = 0; i < m; i++) {
+      int u, v, net; 
+      long long weight; 
+      std::cin >> u >> v >> net >> weight;
+      net--; 
+      listEdges[net].push_back((edge) {u, v, weight});  
+    }
+    for (int net = 0; net < k; net++) {
+      std::cin >> s[net];
+    }
+    for (int net = 0; net < k; net++) {
+      solve(net); 
+    }
+    long long ans = 1000000000000007LL; 
+    for (int j = 0; j < (int) listStates.size(); j++) {
+      std::vector <int> state = listStates[j]; 
+      std::vector <edge> e;
+      long long cost = 0LL;
+      for (int net = 0; net < k; net++) {
+        if (state[net] == 1) {
+          cost += s[net];
+          for (int i = 0; i < (int) listImportantEdges[net].size(); i++) {
+            e.push_back((edge) {listImportantEdges[net][i].u, listImportantEdges[net][i].v, listImportantEdges[net][i].weight});
+          } 
+        }
+        else {
+          for (int i = 0; i < (int) listImportantEdges[net].size(); i++) {
+            e.push_back((edge) {listImportantEdges[net][i].u, listImportantEdges[net][i].v, 2LL * listImportantEdges[net][i].weight});
+          }
+        }
+      }
+      std::sort(e.begin(), e.end());
+      for (int i = 1; i <= n; i++) {
+        root[i] = i; 
+      }   
+      for (int i = 0; i < (int) e.size(); i++) {
+        int u = e[i].u, v = e[i].v; 
+        int rootu = findRoot(u);
+        int rootv = findRoot(v);
+        if (rootu != rootv) {
+          root[rootu] = rootv; 
+          cost += e[i].weight; 
+        } 
+      }
+      ans = std::min(ans, cost); 
+    }
+    std::cout << ans << std::endl;
+  }
+  return 0; 
+}
