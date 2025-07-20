23bc26711c8c17d7f4f287babd08149bfebbd576
hoangprodn <thhoang08091999@gmail.com>
2025-07-20 22:48:50 +0700

    2025 final round 1

---
+++ 2025_final_round_1/A.cpp
@@ -0,0 +1,23 @@
+/*
+**************************************************************************
+  	author     : hoangprodn
+  	email  	   : thhoang08091999@gmail.com
+  	local time : 2025-07-20 22:01:42 
+************************************************************************** 
+*/
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;	
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifdef HOANGPRODN_DEBUG
+	freopen("input.in", "r", stdin);
+	freopen("output.out", "w", stdout);
+#endif // HOANGPRODN_DEBUG
+	solve();
+	return 0;
+}
+
