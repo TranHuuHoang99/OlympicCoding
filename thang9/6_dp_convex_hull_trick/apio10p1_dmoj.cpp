f8f3905303209430cf5cef033fabc318c3d604d6
hoangprodn <thhoang08091999@gmail.com>
2025-09-10 21:09:48 +0700

    thang 9 buoi 6 update bai tap convex hull

--- thang9/6_dp_convex_hull_trick/apio10p1_dmoj.cpp
+++ thang9/6_dp_convex_hull_trick/apio10p1_dmoj.cpp
@@ -1,76 +1,92 @@
-#include <iostream>
-#include <vector>
-#include <math.h>
-#include <algorithm>
-#include <utility>
-struct line {
-  long long a, b, denox, numex; 
-};
-std::vector <line> hull; 
-long long f[1000005]; 
-int n; 
-long long sum[1000005];
-long long a, b, c; 
-std::pair <long long, long long> intersectx(const line &l1, const line &l2) {
-  long long deno = l2.b - l1.b; 
-  long long nume = l1.a - l2.a; 
-  if (nume < 0) {
-    deno = -deno;
-    nume = -nume; 
-  }
-  return std::make_pair(deno, nume); 
-}
-long long query(const long long &x) {
-  int lo = 0; 
-  int hi = (int) hull.size() - 1; 
-  int p = -1; 
-  while (lo <= hi) {
-    int mid = (lo + hi) / 2; 
-    if (hull[mid].denox <= x * hull[mid].numex) {
-      p = mid; 
-      lo = mid + 1; 
-    }
-    else {
-      hi = mid - 1; 
-    }
-  }                                      
-  return hull[p].a * x + hull[p].b; 
-}
-bool bad(const line &l1, const line &l2, const line &l3) {
-  std::pair <long long, long long> x12 = intersectx(l1, l2); 
-  std::pair <long long, long long> x13 = intersectx(l1, l3);
-  if (x13.first * x12.second < x12.first * x13.second) {
-    return true; 
-  } 
-  return false;
-} 
+/*
+**************************************************************************
+  	author     : hoangprodn
+  	email  	   : thhoang08091999@gmail.com
+  	local time : 2025-09-10 20:08:03 
+************************************************************************** 
+*/
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
 
-int main () {
-  std::cin >> n >> a >> b >> c;
-  for (int i = 1; i <= n; i++) {
-    long long soldier; 
-    std::cin >> soldier; 
-    sum[i] = sum[i - 1] + soldier; 
-  } 
-  f[1] = a * sum[1] * sum[1] + b * sum[1] + c;
-  hull.push_back((line) {-2LL * a * sum[1], f[1] - b * sum[1] + a * sum[1] * sum[1], -1, 1}); 
-  for (int i = 2; i <= n; i++) {
-    long long x = sum[i];
-    f[i] = 0; 
-    long long ymax = query(x); 
-    if (ymax > 0) {
-      f[i] = ymax; 
-    }
-    f[i] += a * sum[i] * sum[i] + b * sum[i] + c; 
-    line l = (line) {-2LL * a * sum[i], f[i] - b * sum[i] + a * sum[i] * sum[i], -1, 1};
-    while ((int) hull.size() >= 2 && bad(hull[(int) hull.size() - 2], hull.back(), l) == true) {
-      hull.pop_back(); 
-    } 
-    std::pair <long long, long long> xx = intersectx(hull.back(), l); 
-    l.denox = xx.first;
-    l.numex = xx.second;
-    hull.push_back(l); 
-  }
-  std::cout << f[n]; 
-  return 0; 
+struct Node {
+  	ll a, b;
+  	ll product;
+  	ll factor;
+};
+const ll N = 1e6+5;
+int n;
+ll A, B, C;
+ll sum[N];
+ll F[N];
+vector<Node> hull;
+
+pair<ll,ll> intersect(Node line1, Node line2) {
+ 	ll product = line1.b - line2.b;
+ 	ll factor = line2.a - line1.a;
+ 	if (factor < 0ll) {
+ 	 	factor = -factor;
+ 	 	product = -product;
+ 	}
+ 	return {product, factor};
 }
+
+bool isNotImportant(Node preLastLine, Node lastLine, Node new_line) {
+ 	pair<ll,ll> i1 = intersect(preLastLine, lastLine);
+ 	pair<ll,ll> i2 = intersect(preLastLine, new_line);
+ 	return i1.first * i2.second > i2.first * i1.second;
+}
+
+ll query(ll x) {
+ 	int left = 0;
+ 	int right = hull.size()-1;
+ 	int pos = -1;
+ 	while (left <= right) {                                                                                           
+ 	 	int mid = (left+right)>>1;
+ 	 	if (hull[mid].product <= x * hull[mid].factor) {
+ 	 	 	pos = mid;
+ 	 	 	left = mid + 1;
+ 	 	} else {
+ 	 	 	right = mid - 1;
+ 	 	}
+ 	}
+ 	return hull[pos].a * x + hull[pos].b;
+}
+
+void solve(void) {
+	cin >> n >> A >> B >> C;
+	for (int i = 1; i <= n; i++) {
+	    ll temp;
+	    cin >> temp;
+	    sum[i] = sum[i-1] + temp;
+	}	
+	F[1] = A*sum[1]*sum[1] + B*sum[1] + C;
+	hull.push_back((Node){-2ll*A*sum[1], F[1] + A*sum[1]*sum[1] - B*sum[1], -1ll, 1ll});
+	for (int i = 2; i <= n; i++) {
+	 	ll x = sum[i];
+	 	F[i] = 0ll;
+	 	ll temp = query(x);
+	 	if (temp > 0ll) {
+	 	 	F[i] = temp;
+	 	}
+	 	F[i] += A*sum[i]*sum[i] + B*sum[i] + C;
+	 	Node new_line = (Node){-2ll * A * sum[i], F[i] + A*sum[i]*sum[i] - B*sum[i], -1ll, 1ll};
+	 	while (hull.size() >= 2 && isNotImportant(hull[(int)hull.size()-2], hull.back(), new_line) == true) {
+	 	 	hull.pop_back();
+	 	}
+	 	pair<ll,ll> isect = intersect(hull.back(), new_line);
+	 	new_line.product = isect.first;
+	 	new_line.factor = isect.second;
+	 	hull.push_back(new_line);
+	}
+	cout << F[n] << endl;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	solve();
+	return 0;
+}
+
