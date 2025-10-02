9c2c5fa88b1ac301a6000c4582f928eed5c2ed53
hoangprodn <thhoang08091999@gmail.com>
2025-10-02 15:22:19 +0700

    update bai tap buoi 5

--- thang10/5_heavy_light_decomposition/epic_tree_hackerrank.cpp
+++ thang10/5_heavy_light_decomposition/epic_tree_hackerrank.cpp
@@ -1,153 +1,251 @@
-/*
-**************************************************************************
-  	author     : hoangprodn
-  	email  	   : thhoang08091999@gmail.com
-  	local time : 2025-09-29 21:53:18 
-************************************************************************** 
-*/
-#include <bits/stdc++.h>
-#define ll long long
-using namespace std;
-
-const ll N = 1e5+10;
-int n, q;
-vector<int> adj[N];
-// heavy light decomposition
+#include <iostream> 
+#include <vector>
+#include <math.h>
+#include <algorithm>  
+#include <utility> 
+const long long mod = 10009LL; 
+int n, numQueries; 
+std::vector <int> adj[100005];
+int sz[100005];
+int parent[100005];
+int f[100005][20];
+int height[100005];
+int position[100005];
+int chainStart[100005];
+int chainIndex[100005];
 int chain = 1;
-int chainStart[N];
-int chainIndex[N];
-int position[N];
-int sz[N];
-int parent[N];
-vector<int> listVertex;
-// end
-int dist[N];
-int F[N][20];
-void HLD(int u, int father) {
- 	if (chainStart[chain] == -1) {
- 	 	chainStart[chain] = u;
- 	}
- 	chainIndex[u] = chain;
- 	listVertex.push_back(u);
- 	position[u] = (int)listVertex.size();
-	int special = -1;
-	for (int v : adj[u]) {
-	 	if (v == father) continue;
-	 	if (special == -1 || sz[special] < sz[v]) {
-	 	 	special = v;
-	 	}
-	}
-	if (special != -1) {
-	 	HLD(special, u);
-	}
-	for (int v : adj[u]) {
-	 	if (v == father || v == special) continue;
-	 	chain++;
-	 	HLD(v, u);
-	}
+std::vector <int> listVertex;    
+struct itNode {
+  long long sum = 0LL; 
+  long long lazy = 0LL; 
+  long long sumOfSubtree = 0LL; 
+  long long lazyOfSubtree = 0LL; 
+  itNode() {
+    sum = 0LL; 
+    lazy = 0LL; 
+    sumOfSubtree = 0LL; 
+  }
+};
+itNode it[4 * 100005]; 
+int farthest[100005]; 
+long long prefixSum[100005]; 
+
+void init() {
+  for (int i = 1; i <= n; i++) {
+    chainStart[i] = -1; 
+    for (int j = 0; j <= 17; j++) {
+      f[i][j] = -1; 
+    }
+  }
 }
+
 void dfs(int u, int father) {
- 	parent[u] = father;
- 	sz[u] = 1;
- 	F[u][0] = father;
- 	for (int v : adj[u]) {
- 	 	if (v == father) continue;
- 	 	dist[v] = dist[u] + 1;
- 	 	dfs(v, u);
- 	 	sz[u] += sz[v];
- 	}
+  sz[u] = 1; 
+  f[u][0] = father; 
+  parent[u] = father; 
+  for (int i = 0; i < (int) adj[u].size(); i++) {
+    int v = adj[u][i];
+    if (v == father) {
+      continue; 
+    }
+    height[v] = height[u] + 1; 
+    dfs(v, u); 
+    sz[u] += sz[v]; 
+  }
 }
-void build_ancestor(void) {
- 	for (int j = 1; j <= 19; j++) {
- 	 	for (int i = 1; i <= n; i++) {
- 	 	 	int ancestor = F[i][j-1];
- 	 	 	if (ancestor != -1 && F[ancestor][j-1] != -1) {
- 	 	 	 	F[i][j] = F[ancestor][j-1];
- 	 	 	}
- 	 	}
- 	}
+
+void initLCA() {
+  for (int j = 1; j <= 17; j++) {
+    for (int u = 1; u <= n; u++) {
+      if (f[u][j - 1] == -1) {
+        continue; 
+      }
+      f[u][j] = f[f[u][j - 1]][j - 1]; 
+    }
+  }
 }
+
 int lca(int u, int v) {
- 	if (dist[u] < dist[v]) swap(u, v);
- 	for (int j = 19; j >= 0; j--) {
- 	 	if (dist[u] - (1 << j) >= dist[v]) {
- 	 	 	u = F[u][j];
- 	 	}
- 	}
- 	if (u == v) return u;
- 	for (int j = 19; j >= 0; j--) {
- 	 	if (F[u][j] != -1 && F[v][j] != -1 && F[u][j] != F[v][j]) {
- 	 	 	u = F[u][j];
- 	 	 	v = F[v][j];
- 	 	}
- 	}
- 	return F[u][0];
+  if (height[u] < height[v]) {
+    std::swap(u, v); 
+  }
+  for (int k = 17; k >= 0; k--) {
+    if (height[u] - (1 << k) >= height[v]) {
+      u = f[u][k]; 
+    }
+  }
+  if (u == v) {
+    return u; 
+  }
+  for (int k = 17; k >= 0; k--) {
+    if (f[u][k] != -1 && f[u][k] != f[v][k]) {
+      u = f[u][k];
+      v = f[v][k]; 
+    }
+  }
+  return f[u][0]; 
 }
 
-void solve(void) {
-	
+void heavyLightDecomposition(int u, int father) {
+  if (chainStart[chain] == -1) {
+    chainStart[chain] = u; 
+  }
+  chainIndex[u] = chain; 
+  listVertex.push_back(u);
+  position[u] = (int) listVertex.size(); 
+  int specialVertex = -1; 
+  for (int i = 0; i < (int) adj[u].size(); i++) {
+    int v = adj[u][i];
+    if (v == father) {
+      continue; 
+    }
+    if (specialVertex == -1 || sz[specialVertex] < sz[v]) {
+      specialVertex = v; 
+    }
+  }
+  if (specialVertex != -1) {
+    heavyLightDecomposition(specialVertex, u); 
+  }
+  for (int i = 0; i < (int) adj[u].size(); i++) {
+    int v = adj[u][i];
+    if (v == father) {
+      continue; 
+    }
+    if (v != specialVertex) {
+      chain++; 
+      heavyLightDecomposition(v, u); 
+    }
+  }
 }
 
-int main(void) {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	solve();
-	return 0;
+void createFarthest(int u, int father) {
+  farthest[u] = position[u]; 
+  for (int i = 0; i < (int) adj[u].size(); i++) {
+    int v = adj[u][i];
+    if (v == father) {
+      continue; 
+    }
+    createFarthest(v, u); 
+    farthest[u] = std::max(farthest[u], farthest[v]); 
+  }
 }
 
+void lazyUpdate(int index, int L, int R) {
+  if (it[index].lazy > 0 && L < R) {
+    int mid = (L + R) / 2; 
+    (it[2 * index].lazy += it[index].lazy) %= mod; 
+    (it[2 * index].sum += (1LL * (mid - L + 1) * it[index].lazy) % mod) %= mod; 
+    (it[2 * index].sumOfSubtree += (1LL * ((prefixSum[mid] - prefixSum[L - 1] + mod) % mod) * it[index].lazy) % mod) %= mod; 
+    (it[2 * index + 1].lazy += it[index].lazy) %= mod; 
+    (it[2 * index + 1].sum += (1LL * (R - mid) * it[index].lazy) % mod) %= mod;
+    (it[2 * index + 1].sumOfSubtree += (1LL * ((prefixSum[R] - prefixSum[mid] + mod) % mod) * it[index].lazy) % mod) %= mod; 
+  }
+  it[index].lazy = 0LL; 
+}
 
+void update(int index, int L, int R, int l, int r, int cost) {
+  if (l > R || L > r) {
+    return; 
+  }  
+  if (l <= L && R <= r) {
+    (it[index].lazy += cost) %= mod;
+    (it[index].sum += (1LL * (R - L + 1) * cost) % mod) %= mod; 
+    (it[index].sumOfSubtree += (1LL * ((prefixSum[R] - prefixSum[L - 1] + mod) % mod) * cost) % mod) %= mod;
+    lazyUpdate(index, L, R);
+    return;    
+  }
+  lazyUpdate(index, L, R); 
+  int mid = (L + R) / 2; 
+  update(2 * index, L, mid, l, r, cost);
+  update(2 * index + 1, mid + 1, R, l, r, cost);
+  it[index].sum = (it[2 * index].sum + it[2 * index + 1].sum) % mod;
+  it[index].sumOfSubtree = (it[2 * index].sumOfSubtree + it[2 * index + 1].sumOfSubtree) % mod;
+}
 
+std::pair <long long, long long> get(int index, int L, int R, int l, int r) {
+  if (l > R || L > r) {
+    return std::make_pair(0LL, 0LL); 
+  }
+  if (l <= L && R <= r) {
+    return std::make_pair(it[index].sum, it[index].sumOfSubtree);   
+  }
+  lazyUpdate(index, L, R); 
+  int mid = (L + R) / 2; 
+  std::pair <long long, long long> getLeft = get(2 * index, L, mid, l, r);
+  std::pair <long long, long long> getRight = get(2 * index + 1, mid + 1, R, l, r);
+  std::pair <long long, long long> ret; 
+  ret.first = (getLeft.first + getRight.first) % mod; 
+  ret.second = (getLeft.second + getRight.second) % mod; 
+  return ret; 
+}
 
+void updatePath(int u, int ancestor, int cost) {
+  while (chainIndex[u] != chainIndex[ancestor]) {
+    int startVertex = chainStart[chainIndex[u]]; 
+    update(1, 1, n, position[startVertex], position[u], cost);
+    u = startVertex; 
+    u = parent[u]; 
+  }
+  if (u != ancestor) {
+    update(1, 1, n, position[ancestor] + 1, position[u], cost); 
+  }
+}
 
+std::pair <long long, long long> queryPath(int u, int ancestor) {
+  std::pair <long long, long long> ret = std::make_pair(0LL, 0LL); 
+  while (chainIndex[u] != chainIndex[ancestor]) {
+    int startVertex = chainStart[chainIndex[u]]; 
+    std::pair <long long, long long> twoSums = get(1, 1, n, position[startVertex], position[u]); 
+    (ret.first += twoSums.first) %= mod;
+    (ret.second += twoSums.second) %= mod; 
+    u = startVertex; 
+    u = parent[u]; 
+  }
+  std::pair <long long, long long> twoSums = get(1, 1, n, position[ancestor], position[u]); 
+  (ret.first += twoSums.first) %= mod;
+  (ret.second += twoSums.second) %= mod; 
+  return ret; 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int main () {
+  std::cin >> n;
+  for (int i = 1; i < n; i++) {
+    int u, v;
+    std::cin >> u >> v;
+    adj[u].push_back(v); 
+    adj[v].push_back(u); 
+  }
+  init(); 
+  dfs(1, -1); 
+  initLCA(); 
+  heavyLightDecomposition(1, -1);
+  for (int i = 1; i <= n; i++) {
+    prefixSum[i] = (prefixSum[i - 1] + sz[listVertex[i - 1]]) % mod; 
+  }
+  createFarthest(1, -1);
+  std::cin >> numQueries;
+  for (int query = 1; query <= numQueries; query++) {
+    int type;
+    std::cin >> type; 
+    if (type == 1) {
+      int u, v, cost; 
+      std::cin >> u >> v >> cost;
+      int ancestor = lca(u, v); 
+      updatePath(u, ancestor, cost); 
+      updatePath(v, ancestor, cost); 
+      update(1, 1, n, position[ancestor], position[ancestor], cost); 
+    }
+    else {
+      int u;
+      std::cin >> u;
+      long long ret = 0LL;
+      std::pair <long long, long long> twoSums = queryPath(u, 1);
+      (ret += (1LL * twoSums.first * sz[u]) % mod) %= mod;
+      int lastPosition = farthest[u];
+      twoSums = get(1, 1, n, position[u] + 1, lastPosition);
+      (ret += twoSums.second) %= mod;
+      std::cout << ret << std::endl;    
+    }
+  }
+  return 0; 
+}
