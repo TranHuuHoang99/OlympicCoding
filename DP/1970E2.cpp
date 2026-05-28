a13f4a856ccc414a7d72853f5f369f97843ce234
HoangTranHuu <thhoang08091999@gmail.com>
2026-05-28 15:37:24 +0700

    DP 1970E2

---
+++ DP/1970E2.cpp
@@ -0,0 +1,26 @@
+/*
+**************************************************************************
+    author     : hoangprodn
+    email  	   : thhoang08091999@gmail.com
+    local time : 2026-05-28 15:17:31
+**************************************************************************
+*/
+#include <bits/stdc++.h>
+#define int long long
+#define pi acos(-1.0)
+using namespace std;
+
+void solve(void) {
+
+}
+signed main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
+
+
+
+
+
