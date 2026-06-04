5c170d552e9f586b131e431da8d3d568c6cf173e
HoangTranHuu <thhoang08091999@gmail.com>
2026-06-04 22:27:49 +0700

    update approach for 1970E2

--- DP/1970E2.cpp
+++ DP/1970E2.cpp
@@ -10,8 +10,16 @@
 #define pi acos(-1.0)
 using namespace std;
 
+struct Node {
+    int s_trail, l_trail;
+};
+const int MOD = 1e9+7;
+int m, n;
+Node A[110];
 void solve(void) {
-
+    cin >> m >> n;
+    for (int i = 1; i <= m; i++) cin >> A[i].s_trail;
+    for (int i = 1; i <= m; i++) cin >> A[i].l_trail;
 }
 signed main(void) {
     ios::sync_with_stdio(false);
