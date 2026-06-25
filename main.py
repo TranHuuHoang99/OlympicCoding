0366184160b0f2eaf1ac1df25af3e290e540ff99
HoangTranHuu <thhoang08091999@gmail.com>
2026-06-25 21:19:01 +0700

    update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 **************************************************************************
     author     : hoangprodn
     email      : thhoang08091999@gmail.com
-    local time : 2026-06-18 15:24:57
+    local time : 2026-06-25 21:18:00
 **************************************************************************
 """
 from collections import deque, Counter, defaultdict
@@ -10,29 +10,13 @@ import heapq
 import bisect
 import math
 import sys
+import os
 from functools import cmp_to_key
-# set recursion limit
-sys.setrecursionlimit(int(2e5))
-# handle input
 # ----------------------------------------------------
 input = sys.stdin.readline
-parts = input().split()
-n = int(parts[0])
-d = int(parts[1])
-dq = deque()
-for e in input().split():
-    dq.append(e)
 # ----------------------------------------------------
 
 def solve():
-    for _ in range(d):
-        if dq:
-            val = dq[0]
-            dq.popleft()
-            dq.append(val)
-    while dq:
-        print(dq[0], end=" ")
-        dq.popleft()
     pass
 
 if __name__ == "__main__":
